1655969d9011018567d655eeb21ca2aeb14feec5
Martin <martin.rivard@teksavvy.com>
2022-11-19 15:13:35 -0500

    Implement the basic layout manager.

--- src/main/java/com/CodeSmell/LayoutManager.java
+++ src/main/java/com/CodeSmell/LayoutManager.java
@@ -1,20 +1,42 @@
 package com.CodeSmell;
 
 import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.Collections;
-import java.util.Arrays;
+import java.lang.reflect.Array;
 
 import com.CodeSmell.UMLClass;
-import com.CodeSmell.Position;
-import com.CodeSmell.RenderEvent;
-import com.CodeSmell.Shape;
+
+
 
 class LayoutManager  {
 
-	public LayoutManager() {}
+	private enum Direction {
+		RIGHT,
+		LEFT;
+		public Direction opposite(){
+			switch(this){
+				case RIGHT: return Direction.LEFT;
+				case LEFT: return Direction.RIGHT;
+				default: return Direction.RIGHT;
+			}
+		}
+	}
 
-	private ArrayList<UMLClass> classes;
+	private static double Y_PADDING = 30; //size of gap between classes vertically
+	private static double RELATION_GRID_SIZE = 0.4; 
+	private static double MIDDLE_SPACING_SIZE = 0.3; //how much of the gap in width can be filled up by relations
+	private static double PADDING_DIVISOR = 2; //how much padding (gap in width between two columns), as a divisor
+
+	private ArrayList<UMLClass>[] grid;
+	private ArrayList<ClassRelation> relations;
+
+	public LayoutManager(ArrayList<UMLClass> classes, ArrayList<ClassRelation> relations) {
+
+		this.positionClasses(classes);
+		this.setSpacing();
+		
+		this.relations = relations;
+		this.setRelationPaths();
+	}
 
 	public void positionClasses(ArrayList<UMLClass> classes) {
 		/**
@@ -22,263 +44,270 @@ class LayoutManager  {
 		 * set their position with the classes setPosition() method
 		 */
 
-		this.classes = classes;
-		sortClasses();
+		// determine coordinates for starting c
+		//int x = 0;
+		//int y = 0;
+		
+		// Find the number of columns that this list of classes will turn into.
+		int nColumns = (int) Math.ceil(Math.sqrt(classes.size()));
 
+		//Generate the grid
+		ArrayList<UMLClass>[] grid = new ArrayList[nColumns];
+		for(int i = 0; i < nColumns; i++){
+			grid[i] = new ArrayList<UMLClass>();
+		}
+
+		//keep track of which column we're on
+		int curColumn = 0;
+		///Populate the grid
+		for(int i = 0; i < classes.size(); i++){
+			if(curColumn == nColumns){
+				curColumn = 0;
+			}
+			grid[curColumn].add(classes.get(i));
+			curColumn++;
+		}
+		this.grid = grid;
+	}
+
+
+	/**
+	* Spaces out the positions of the classes in this.grid evenly.
+	*
+	*/
+	private void setSpacing(){
+		//First, iterate over each column and set x
 		double x = 0;
-		double y = 0;
-
-		for (int i=0; i < classes.size(); i++) {
-			// set the classes's ith class' 
-			// x position to be after i-1th's with
-			// some padding. 
-			if (i >= 1) {
-				UMLClass lastClass = classes.get(i - 1);
-				Double lastX = lastClass.getPosition().x;
-				Double lastWidth = classes.get(i - 1).getWidth();
-				Double padding = lastWidth / 2;
+		double y;
+		for(int col = 0; col < this.grid.length; col++){
+			y = 0;
+			if (col > 0){
+				double lastX = this.grid[col-1].get(0).getPosition().x;
+				double lastWidth = maxWidth(col-1);
+				double padding = lastWidth / 2;
 				x = lastX + lastWidth + padding;
-				Double lastY = lastClass.getPosition().y;
-				y = lastY + classes.get(i - 1).getHeight();
-
 			}
-			classes.get(i).setPosition(x, y);
-		}
-	}
-
-	private class Line extends Shape {
-		/* Represents a line (i.e, one connecting
-		two classes) */
-
-		public final double x1, x2, y1, y2;
-		
-		Line(Position p1, Position p2) {
-			super(new Position[]{p1, p2}, "#4C94A2");
-			this.x1 = p1.x;
-			this.x2 = p2.x;
-			this.y1 = p1.y;
-			this.y2 = p2.y;	
-		}
-
-		Line(double x1, double y1, double x2, double y2) {
-			super(new Position[] {
-					new Position(x1, y1), new Position(x2, y2)
-				}, "orange");
-			this.x1 = x1;
-			this.x2 = x2;;
-			this.y1 = y1;
-			this.y2 = y2;	
-		}
-
-		public String toString() {
-			return String.format("(%f, %f) -> (%f, %f)", 
-					x1, y1, x2, y2);
-		}
-
-		public double magnitude() {
-			return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
-		}
-	}
-
-	private class Dot extends Shape {
-		/* A dot to be rendered for debugging purposes */
-
-		Dot(Position p, String colour) {
-			super(new Position[] { p }, colour);
-		}
-	}
-
-	private Line[] classBoxLines(UMLClass c) {
-		Line[] lines = new Line[4];
-		Position p1 = c.getPosition();
-		Position corner2 = new Position(p1.x + c.getWidth(), p1.y);
-		Position corner3 = new Position(p1.x + c.getWidth(), p1.y + c.getHeight());
-		Position corner4 = new Position(p1.x, p1.y + c.getHeight());
-		lines[0] = new Line(p1, corner2);
-		lines[1] = new Line(corner2, corner3);
-		lines[2] = new Line(corner3, corner4);
-		lines[3] = new Line(corner4, p1);
-		return lines;
-	}
-
-	private boolean testCollision(Line line, UMLClass[] exclude) {
-		/* returns true if the given line does not
-		 hit the bounding box of any class not in exclude */
-
-		for (UMLClass c : classes) {
-			if (Arrays.asList(exclude).contains(c)) {
-				continue;
-			}
-			if (intersection(line, classBoxLines(c)) != null) {
-				return false;
-			}
-		}
-		return true;
-	}
-
-	private Position intersection(Line l1, Line[] with) {
-		/* gets the Position of where l1 interesects with a line in with */
-
-		for (Line l2 : with) {
-			double x1 = l1.x1;
-			double x2 = l1.x2;
-			double x3 = l2.x1;
-			double x4 = l2.x2;
-			double y1 = l1.y1;
-			double y2 = l1.y2;
-			double y3 = l2.y1;
-			double y4 = l2.y2;
-			double a = ((x4-x3)*(y1-y3) - (y4-y3)*(x1-x3)) / 
-					((y4-y3)*(x2-x1)  - (x4-x3)*(y2-y1));
-			double b = ((x2-x1)*(y1-y3) - (y2-y1)*(x1-x3)) / 
-					((y4-y3)*(x2-x1)  - (x4-x3)*(y2-y1));
-			if (a >= 0 && a <= 1 &&  b >= 0 && b <= 1) {
-				return new Position(x1 + a*(x2-x1), y1 + a*(y2-y1));
-			}
-		}
-		return null;
-	}
-
-	private enum Direction {
-		TOP,
-		BOTTOM,
-		LEFT, 
-		RIGHT
-	}
-
-	private Position classMiddle(UMLClass c) {
-		/* Gets the position of the middle of a class bounding box */
-
-		Position p = c.getPosition(); // topleft corner of c
-		return new Position(p.x + c.getWidth() / 2, p.y + c.getHeight() / 2);
-	}
-
-	private Position classMiddle(UMLClass c, Direction d) {
-		/* Gets the position of the middle of the given edge (direction)
-		 of a class bounding box */
-
-		Position p = c.getPosition(); // topleft corner of c
-		if (d == Direction.TOP) {
-			return new Position(p.x + c.getWidth() / 2, p.y);
-		} else if (d == Direction.BOTTOM) {
-			return new Position(p.x + c.getWidth() / 2, p.y + c.getHeight());
-		} else if (d == Direction.LEFT) {
-			return new Position(p.x, p.y + c.getHeight() / 2);
-		} else if (d == Direction.RIGHT) {
-			return new Position(p.x + c.getWidth(), p.y + c.getHeight() / 2);
-		} else {
-			throw new RuntimeException("Bad direction");
-		}
-	}
-
-	private Pair<Position, Position> closetNodes(UMLClass c1, UMLClass c2) {
-		/* 
-		returns the two closest points relations that sit on 
-		each classes box boundry to connect the two clases 
-		*/
-
-		// get mid point of each class
-		Position mid1 = classMiddle(c1);
-		Position mid2 = classMiddle(c2);
-
-		// return the position of where a line connecting the midpoints
-		// intersects with the class box boundaries
-		Line line = new Line(mid1, mid2);
-		Position p1, p2;
-		p1 = intersection(line, classBoxLines(c1));
-		p2 = intersection(line, classBoxLines(c2));
-		if (p1 == null || p2 == null) {
-			throw new RuntimeException("No intersection");
-		}
-		return new Pair<Position, Position>(p1, p2);
-	} 
-
-	private Position indirectConnection(UMLClass c1, UMLClass c2) {
-		/* gets the  position of a bend where the perpendicular lines
-		coming from the midpoints of c1 and c2 intersect */
-
-		// get top left corners of each class 
-		Position mid1 = classMiddle(c1);
-		Position mid2 = classMiddle(c2);
-
-		// new Dot(mid1, "blue").render();
-		// new Dot(mid2, "red").render();
-		Line l1 = new Line(mid1.x, mid1.y, mid2.x, mid1.y);
-		Line l2 = new Line(mid2.x, mid2.y, mid2.x, mid1.y);
-		Position p = intersection(l1, new Line[] { l2 });
-		//l2.render();
-		if (p == null) {
-			throw new RuntimeException("No intersection");
-		}
-		return p;
-	}
-
-	private void sortClasses() {
-		// sorts the classes such that the ones which are furthest left are first
-
-		Comparator<UMLClass> classHorzComp = new Comparator<UMLClass>() {
-	        public int compare(UMLClass c1, UMLClass c2) {
-	        	Double d = (c1.getPosition().x - c2.getPosition().x);
-	            return d.intValue();
-	        }
-	    };
-	    
-	    Collections.sort(classes, classHorzComp);
-	}
-
-	public void setRelationPaths(ArrayList<ClassRelation> relations) {
-		/**
-		 * given a list of classes which have been rendered and 
-		 * positioned, set the path for their index of their connections
-		 * attribute.
-		 *  
-		 */
-
-		for (ClassRelation r : relations) {
-			ArrayList<Position> path = new ArrayList<Position>();
-		
-			Pair<Position, Position> p = closetNodes(r.source, r.target);
-			Position terminal = p.second;
-			Position test = p.first; 
-			boolean collision = !testCollision(
-					new Line(test, terminal), new UMLClass[]{ r.source, r.target });
-
-			if (collision) {
-				System.out.printf("Indirect path from %s to %s\n", 
-						r.source.name, r.target.name);
-		
-				Position bend, mid1, mid2;
-				bend = indirectConnection(r.source, r.target);
-				mid1 = classMiddle(r.source);
-				mid2 = classMiddle(r.target);
-
-				if (bend.y > mid1.y && bend.x < mid2.x) {
-					mid1 = classMiddle(r.source, Direction.BOTTOM);
-					mid2 = classMiddle(r.target, Direction.LEFT);
-				} else if (bend.x < mid1.x && bend.y > mid2.y) {
-					mid1 = classMiddle(r.source, Direction.LEFT);
-					mid2 = classMiddle(r.target, Direction.BOTTOM);
-				} else if (bend.x > mid2.x && bend.y < mid1.y) {
-					mid1 = classMiddle(r.source, Direction.TOP);
-					mid2 = classMiddle(r.target, Direction.RIGHT);
-				} else if (bend.x > mid1.x && bend.y < mid2.y) {
-					mid1 = classMiddle(r.source, Direction.RIGHT);
-					mid2 = classMiddle(r.target, Direction.TOP);
+			//Then iterate through each column and set y
+			for(int i = 0; i < this.grid[col].size(); i++){
+				if(i > 0){
+					y = y + this.grid[col].get(i-1).getHeight() + Y_PADDING;
 				}
-
-				path.add(mid1);
-				path.add(bend);
-				path.add(mid2);
-				System.out.println(path);
-				r.setPath(path);
-			} else {
-				path.add(test);
-				System.out.printf("Direct path from %s to %s\n", 
-						r.source.name, r.target.name);
-				path.add(terminal);
-				System.out.println(path);
-				r.setPath(path);
+				this.grid[col].get(i).setPosition(x, y);
 			}
 		}
+		
 	}
+
+	/**
+	* Self-explanatory, gets the ceiling of the width of a list of classes.
+	*
+	*/
+	private double maxWidth(int col){
+		double max = 0;
+		double width;
+		for(int n = 0; n < this.grid[col].size(); n++){
+			width = this.grid[col].get(n).getWidth();
+			if(width > max){
+				max = width;
+			}
+		}
+		return max;
+	}
+
+	/**
+	* 
+	*/
+	private void setRelationPaths(){
+		for(ClassRelation r : this.relations){
+			
+			ArrayList<Position> path = new ArrayList<Position>();
+			int curColumn = getColumn(r.source); 
+			int destColumn = getColumn(r.target);
+			Direction dir = getRelationDir(r);
+			double padding = maxWidth(curColumn)/(PADDING_DIVISOR*2);
+			
+
+			Position start = getStartPoint(r, r.source, dir);
+			path.add(start); //Path start
+			
+			if(getRelationDir(r) == Direction.RIGHT)
+				path.add(new Position(getPaddedPoint(r, curColumn), start.y));
+			else
+				path.add(new Position(getPaddedPoint(r, curColumn-1), start.y));
+
+			Position end;
+			
+			if(r.source.getPosition().x == r.target.getPosition().x){
+				//special case: starts and ends in the same column
+				end = getStartPoint(r, r.target, Direction.RIGHT);
+				path.add(new Position(getPaddedPoint(r, curColumn), end.y));
+				//path.add(new Position((r.source.getPosition().x + (padding * 5)), end.y));
+				
+			}else{
+			//Add intermediate path waypoints..
+			while(Math.abs(curColumn - destColumn) > 1){
+				addPassthrough(r, path, curColumn, destColumn);
+				if(dir == Direction.RIGHT){ 
+					curColumn++;
+				}else{ 
+					curColumn--;
+				}
+			}
+			
+			end = getStartPoint(r, r.target, this.getRelationDir(r).opposite());
+			if(getRelationDir(r) == Direction.RIGHT)
+				path.add(new Position(getPaddedPoint(r, curColumn), end.y));
+			else
+				path.add(new Position(getPaddedPoint(r, curColumn-1), end.y));
+			
+			}
+			path.add(end);
+			r.setPath(path);
+		}
+	}
+
+	private void addPassthrough(ClassRelation r, ArrayList<Position> p, int startCol, int targetCol){
+		
+		double[] gapPositions = new double[grid[targetCol].size()];
+		for(int n = 0; n < grid[targetCol].size(); n++){
+			gapPositions[n] = grid[targetCol].get(n).getPosition().y 
+				+ grid[targetCol].get(n).getHeight() 
+				+ Y_PADDING/2;
+			
+		}
+		double startX = p.get(p.size()-1).x;
+		double startY = p.get(p.size()-1).y;
+		
+		double targetY = gapPositions[0];
+		double targetX;
+		if(targetCol < startCol)
+			targetX = getPaddedPoint(r, startCol - 2);
+		else
+			targetX = getPaddedPoint(r, startCol + 1);
+		
+		for(double n : gapPositions){
+			if(Math.abs(n - startY) < Math.abs(targetY - startY))
+				targetY = n;
+		}
+		//System.out.println("startX: " + startX + ", startY: " + startY);
+		//System.out.println("targetX: " + targetX + ", targetY: " + targetY);
+		p.add(new Position(startX, targetY));
+		p.add(new Position(targetX, targetY));
+		
+	}
+	
+	/**
+	* Get which direction the relation is going horizontally - to the right or no difference (true) or to the left (false).
+	*/
+	private Direction getRelationDir(ClassRelation r){
+		if(r.source.getPosition().x <= r.target.getPosition().x){
+			return Direction.RIGHT;
+		}else{
+			return Direction.LEFT;
+		}
+	}
+
+	/**
+	* Get which column the class is in.
+	*/
+	private int getColumn(UMLClass theClass){
+		for(int n = 0; n < grid.length; n++){
+			if(grid[n].get(0).getPosition().x == theClass.getPosition().x)
+				return n;
+		}
+		return 0;
+	}
+	
+	/**
+	* Gets a point in the middle of the 'padding' gap area directly in front of
+	* the start point.
+	*/
+	private double getPaddedPoint(ClassRelation relation, int theCol){
+		
+		//get number of ClassRelations that pass through the area
+		int nPassthroughs = 0;
+		int index = 0;
+		
+		double startX = grid[theCol].get(0).getPosition().x;
+		
+		for(ClassRelation r: relations){
+			if((startX <= relation.source.getPosition().x) 
+				&& (r.target.getPosition().x >= relation.target.getPosition().x)){
+				nPassthroughs++;
+			} else if ((startX >= relation.source.getPosition().x) 
+				&& (r.target.getPosition().x <= relation.target.getPosition().x)){
+				nPassthroughs++;
+			}
+			if(r == relation)
+				index = relations.indexOf(r);
+		}
+		double colWidth;
+		double distroStart;
+		double distroEnd;
+		
+		//if(getRelationDir(relation) == Direction.RIGHT){ //right
+			colWidth = maxWidth(theCol);
+			distroStart = startX + colWidth + (colWidth/(PADDING_DIVISOR*2)) 
+				- ((colWidth/PADDING_DIVISOR)*MIDDLE_SPACING_SIZE);
+			distroEnd = startX + colWidth + (colWidth/(PADDING_DIVISOR*2)) 
+				+ ((colWidth/PADDING_DIVISOR)*MIDDLE_SPACING_SIZE);
+			/*colWidth = maxWidth(getColumn(relation.source));
+			distroStart = relation.source.getPosition().x + colWidth + (colWidth/(PADDING_DIVISOR*2)) 
+				- ((colWidth/PADDING_DIVISOR)*MIDDLE_SPACING_SIZE);
+			distroEnd = relation.source.getPosition().x + colWidth + (colWidth/(PADDING_DIVISOR*2)) 
+				+ ((colWidth/PADDING_DIVISOR)*MIDDLE_SPACING_SIZE);*/
+				
+		/*} else{ //left
+		
+			colWidth = maxWidth(theCol); 
+			distroStart = startX + colWidth + (colWidth/(PADDING_DIVISOR*2)) 
+				- ((colWidth/PADDING_DIVISOR)*MIDDLE_SPACING_SIZE);
+			distroEnd = startX + colWidth + (colWidth/(PADDING_DIVISOR*2)) 
+				+ ((colWidth/PADDING_DIVISOR)*MIDDLE_SPACING_SIZE);
+			colWidth = maxWidth(getColumn(relation.source)-1);	
+			distroStart = relation.source.getPosition().x - (colWidth/(PADDING_DIVISOR*2)) 
+				- ((colWidth/PADDING_DIVISOR)*MIDDLE_SPACING_SIZE);
+			distroEnd = relation.source.getPosition().x - (colWidth/(PADDING_DIVISOR*2)) 
+				+ ((colWidth/PADDING_DIVISOR)*MIDDLE_SPACING_SIZE);
+		}*/
+		
+		double d = (distroEnd - distroStart)/(nPassthroughs + 1);
+		double x = distroStart + index*d;
+		return x;
+	}
+
+	/**
+	* Get the start position for a given relation that starts from the class
+	*
+	*/
+	private Position getStartPoint(ClassRelation relation, UMLClass theClass, Direction side){
+		ArrayList<ClassRelation> relationList = new ArrayList<ClassRelation>();
+		for(ClassRelation r : relations){
+			if((r.source == theClass && getRelationDir(r) == side)
+				|| (r.target == theClass && getRelationDir(r) == side.opposite())){
+				relationList.add(r);
+			}
+		} 
+		
+		int index = 0;
+		if(relation.source == theClass)
+			index = relationList.indexOf(relation);
+		
+		double distroStart = theClass.getPosition().y + (theClass.getHeight()*0.5 
+				- (theClass.getHeight()*RELATION_GRID_SIZE));
+		double distroEnd = theClass.getPosition().y + (theClass.getHeight()*0.5
+				+ (theClass.getHeight()*RELATION_GRID_SIZE));
+				
+	
+		double d = (distroEnd - distroStart)/(relationList.size() + 1);
+		double y = distroStart + index*d;
+		double x;
+		if(side == Direction.RIGHT){
+			x = theClass.getPosition().x + theClass.getWidth();
+		}else{
+			x = theClass.getPosition().x;
+		}
+		return new Position(x, y);
+	}
+
 }
