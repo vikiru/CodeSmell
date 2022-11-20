d84133f338365bdb09c67bd6afcfdd028c00c9e4
vikiru <viskirubakaran@cmail.carleton.ca>
2022-11-20 03:53:01 -0500

    Mega Refactor joern_query
    - reduced number of joern query calls to one single call for everything needed
    - used regex to split full method body decl to get modifiers, method body and return type of method
    - add a final delete query at the end to delete and close project from user's disk
    - removed any unused methods
    - simplified json creation function to 54 lines instead of 200+

--- src/main/python/joern_query.py
+++ src/main/python/joern_query.py
@@ -1,183 +1,7 @@
 from cpgqls_client import CPGQLSClient, import_code_query
 import os
+import re
 import json
-
-# Execute joern queries to get all the fields of classes with their access modifiers and write to a file.
-def get_fields_with_modifiers():
-
-    # Perform queries to get private, public, protected, and static fields.
-    query = "cpg.member.isPrivate.code.toJsonPretty"
-    result = client.execute(query)
-    data = result["stdout"]
-    privateFields = clean_json(data)
-
-    query = "cpg.member.isPublic.code.toJsonPretty"
-    result = client.execute(query)
-    data = result["stdout"]
-    publicFields = clean_json(data)
-
-    query = "cpg.member.isProtected.code.toJsonPretty"
-    result = client.execute(query)
-    data = result["stdout"]
-    protectedFields = clean_json(data)
-
-    query = "cpg.member.isStatic.code.toJsonPretty"
-    result = client.execute(query)
-    data = result["stdout"]
-    staticFields = clean_json(data)
-
-    # Load JSON into a python list for each field
-    privateArr = json.loads(privateFields)
-    publicArr = json.loads(publicFields)
-    protectedArr = json.loads(protectedFields)
-    staticArr = json.loads(staticFields)
-    return privateArr, publicArr, protectedArr, staticArr
-
-
-# OLD
-# Create a custom JSON object of the source code and save it to a JSON file.
-def source_code_json_creation():
-    # Initial query to get all of the classes within the source code
-    query = "cpg.typeDecl.isExternal(false).toJsonPretty"
-    result = client.execute(query)
-    data = result["stdout"]
-    allClasses = json.loads(clean_json(data))
-
-    # Dictionary which will store everything about the source code.
-    sourceCodeJSON = {}
-    sourceCodeJSON["classes"] = []
-
-    privateArr, publicArr, protectedArr, staticArr = get_fields_with_modifiers()
-    modifiers = ["public", "private", "protected", "static", "final"]
-
-    # Go class by class populating sourceCodeJSON
-    for currClass in allClasses:
-        # Define a dict for each class in the source code
-        className = currClass["name"]
-        if "$" in className:
-            className = className.replace("$", ".")
-        currClassDict = {"name": className, "fields": [], "methods": []}
-
-        # Perform queries to get all of the fields and methods of the class
-        query = (
-            "cpg.typeDecl("
-            + '"'
-            + className
-            + '"'
-            + ").astChildren.isMember.toJsonPretty"
-        )
-        result = client.execute(query)
-        data = result["stdout"]
-        allFields = json.loads(clean_json(data))
-
-        query = (
-            "cpg.typeDecl("
-            + '"'
-            + className
-            + '"'
-            + ").astChildren.isMethod.toJsonPretty"
-        )
-        result = client.execute(query)
-        data = result["stdout"]
-        allMethods = json.loads(clean_json(data))
-
-        # Create dicts for each field and method of each class
-        for currField in allFields:
-            fieldCode = currField["code"]
-            currFieldDict = {
-                "name": currField["name"],
-                "modifiers": [],
-                "type": currField["typeFullName"],
-            }
-
-            # Add the modifiers of the field
-            if fieldCode in privateArr:
-                currFieldDict["modifiers"].append("private")
-            if fieldCode in publicArr:
-                currFieldDict["modifiers"].append("public")
-            if fieldCode in protectedArr:
-                currFieldDict["modifiers"].append("protected")
-            if fieldCode in staticArr:
-                currFieldDict["modifiers"].append("static")
-            currClassDict["fields"].append(currFieldDict)
-
-        for currMethod in allMethods:
-            methodName = currMethod["name"]
-            currMethodDict = {
-                "code": currMethod["code"],
-                "methodBody": "",
-                "modifiers": [],
-                "name": methodName,
-                "instructions": [],
-                "returnType": "",
-            }
-            fullMethodCodeDecl = currMethod["code"]
-            index = fullMethodCodeDecl.find(methodName)
-
-            if index == -1 and methodName == "<init>":
-                newIndex = fullMethodCodeDecl.find("(")
-                result = fullMethodCodeDecl[0:newIndex].split()
-                if len(result) == 2:
-                    currMethodDict["name"] = result[1]
-                elif len(result) == 1:
-                    currMethodDict["name"] = result[0]
-                index = fullMethodCodeDecl.find(currMethodDict["name"])
-
-            modifiersWithReturn = fullMethodCodeDecl[0:index]
-            methodBody = fullMethodCodeDecl[index : len(fullMethodCodeDecl)]
-            methodBodyArr = []
-            methodBodyArr.extend(modifiersWithReturn.split())
-            methodBodyArr.append(methodBody)
-
-            # Extract the modifiers, methodBody, and returnType of a method
-            for currItem in methodBodyArr:
-                if currItem in modifiers:
-                    currMethodDict["modifiers"].append(currItem)
-                else:
-                    if "(" in currItem:
-                        currMethodDict["methodBody"] = currItem
-                    else:
-                        currMethodDict["returnType"] = currItem
-
-            # Execute a query to get the children of each method (aka the instructions and calls)
-            query = (
-                "cpg.method("
-                + '"'
-                + methodName
-                + '"'
-                + ").where(node => node.astParent.isTypeDecl.name("
-                + '"'
-                + className
-                + '"'
-                + ")).astChildren.astChildren.toJsonPretty"
-            )
-            result = client.execute(query)
-            data = result["stdout"]
-            allInstructions = json.loads(clean_json(data))
-
-            # Go instruction by instruction for each method and append to currMethodDict
-            def create_instruction_dict(instruction):
-                currInstructionDict = {
-                    "_label": instruction["_label"],
-                    "code": instruction["code"],
-                    "lineNumber": instruction.get("lineNumber", None),
-                }
-                return currInstructionDict
-
-            currMethodDict["instructions"] = list(
-                map(create_instruction_dict, allInstructions)
-            )
-
-            # Append the currMethodDict to currClassDict["methods"] list
-            currClassDict["methods"].append(currMethodDict)
-
-        # Add the class dict to the main sourceCodeJSON dict
-        sourceCodeJSON["classes"].append(currClassDict)
-
-    # Finally, write everything to a file.
-    joernResult = json.dumps(sourceCodeJSON)
-    write_to_file(joernResult, "sourceCode.json")
-
 
 # Cleans up json output from joern so that it can be a proper JSON file.
 def clean_json(joernResult):
@@ -188,24 +12,62 @@ def clean_json(joernResult):
     return joernResult
 
 
-def new_source_code_json_creation():
+# Execute a single joern query to get all the data required to create a json representation of the source code.
+def source_code_json_creation():
+    # Obtain the classes, fields and modifiers, methods and their instructions
     query = "cpg.typeDecl.isExternal(false).map(node => (node.name, node.astChildren.isMember.l.map(node => (node, node.astChildren.isModifier.modifierType.l)), node.astChildren.isMethod.l.map(node => (node, node.astChildren.astChildren.l)))).toJsonPretty"
     result = client.execute(query)
-    data = result["stdout"]
-    allData = json.loads(clean_json(data))
-    sourceCodeJSON = {"classes": []}
+    allData = json.loads(clean_json(result["stdout"]))
 
+    # For every field, create a dictionary and return it.
+    def create_field_dict(currField):
+        currFieldDict = {
+            "name": currField["_1"]["name"],
+            "modifiers": [x.lower() for x in currField["_2"]],
+            "type": currField["_1"]["typeFullName"],
+        }
+        return currFieldDict
+
+    # For every method, create a dictionary and return it.
+    def create_method_dict(currMethod):
+        # For every method's instruction, create a dictionary and return it.
+        def create_instruction_dict(currInstruction):
+            currInstructionDict = {
+                "_label": currInstruction["_label"],
+                "code": currInstruction["code"].replace("\r\n", ""),
+                "lineNumber": currInstruction.get("lineNumber", None),
+            }
+            return currInstructionDict
+
+        # Get the modifiers, return type and the method body from the full method body provided by Joern.
+        modifiersPattern = re.compile("(private|public|protected|static|final)")
+        returnTypePattern = re.compile("(^[a-zA-z]*)")
+        methodBody = re.sub(modifiersPattern, "", currMethod["_1"]["code"])
+        methodBody = re.sub(returnTypePattern, "", methodBody)
+
+        currMethodDict = {
+            "code": currMethod["_1"]["code"],
+            "methodBody": methodBody.strip(),
+            "modifiers": modifiersPattern.findall(currMethod["_1"]["code"]),
+            "name": currMethod["_1"]["name"],
+            "instructions": list(map(create_instruction_dict, currMethod["_2"])),
+            "returnType": returnTypePattern.findall(currMethod["_1"]["code"]),
+        }
+        return currMethodDict
+
+    # For every class, create a dictionary and return it.
     def create_class_dict(currClass):
         currClassDict = {
-            "name": currClass["_1"].replace("$", "."),
-            "fields": currClass["_2"],
-            "methods": currClass["_3"],
+            "name": currClass["_1"],
+            "fields": list(map(create_field_dict, currClass["_2"])),
+            "methods": list(map(create_method_dict, currClass["_3"])),
         }
         return currClassDict
 
-    sourceCodeJSON["classes"] = list(map(create_class_dict, allData))
-    joernResult = json.dumps(sourceCodeJSON)
-    write_to_file(joernResult, "sourceCode_new.json")
+    # Create a dictionary with all of the info about the source code and write it to a .json file.
+    sourceCodeJSON = {"classes": list(map(create_class_dict, allData))}
+    joernResult = json.dumps(sourceCodeJSON, indent=4)
+    write_to_file(joernResult, "sourceCode.json")
 
 
 # Write the joern output of a query to a specified filename
@@ -224,9 +86,10 @@ if __name__ == "__main__":
     # which will always store the imported source code
     directory = os.getcwd()
     directory = directory.replace("\\", "//")
-    # dirName = directory + "/src/main/python/joernTestProject/src"
+    dirName = directory + "/src/main/python/joernTestProject/src"
     projectName = "analyzedProject"
 
+    # For testing purposes. Get the path of src/main/java/com/CodeSmell as shown (replace '\\' with '/')
     ourProjectDir = (
         "C:/Users/viski/OneDrive/Documents/GitHub/CodeSmell/src/main/java/com/CodeSmell"
     )
@@ -236,20 +99,12 @@ if __name__ == "__main__":
     # originalDir = "D:/SYSC3110/Lab1"
     query = import_code_query(ourProjectDir, projectName)
     result = client.execute(query)
-    new_source_code_json_creation()
-
-"""
-    import cProfile
-
-    cProfile.run("source_code_json_creation()", "output.dat")
-
-    import pstats
-    from pstats import SortKey
-
-    with open("output_time.txt", "w") as f:
-        p = pstats.Stats("output.dat", stream=f)
-        p.sort_stats("time").print_stats()
-    with open("output_calls.txt", "w") as f:
-        p = pstats.Stats("output.dat", stream=f)
-        p.sort_stats("calls").print_stats()
-"""
+    
+    # Create the source code json representation
+    source_code_json_creation()
+    
+    # Close and delete the project from user's bin/joern/joern-cli/workspace 
+    query = 'delete ("' + projectName + '")'
+    result = client.execute(query)
+    if result["success"]:
+        print("The source code has been successfully removed.")
