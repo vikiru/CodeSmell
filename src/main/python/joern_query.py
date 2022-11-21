9026d6700e468b445004b586182f0cb8d1aec48e
vikiru <viskirubakaran@cmail.carleton.ca>
2022-11-21 15:27:07 -0500

    Removed camelCase names and added print statement for import code
    - added additional print for source code import
    - changed method and variable names to be more pythonic and not camelCase
    - added synchronized, volatile, abstract,native to modifiers regex in joern_query

--- src/main/python/joern_query.py
+++ src/main/python/joern_query.py
@@ -5,12 +5,12 @@ from cpgqls_client import CPGQLSClient, import_code_query
 
 
 # Cleans up json output from joern so that it can be a proper JSON file.
-def clean_json(joernResult):
-    stringToFind = '"""'
-    index = joernResult.find(stringToFind)
-    joernResult = joernResult[index : len(joernResult)]
-    joernResult = joernResult.replace('"""', "")
-    return joernResult
+def clean_json(joern_result):
+    string_to_find = '"""'
+    index = joern_result.find(string_to_find)
+    joern_result = joern_result[index : len(joern_result)]
+    joern_result = joern_result.replace('"""', "")
+    return joern_result
 
 
 # Execute a single joern query to get all the data required to create a json representation of the source code.
@@ -22,58 +22,60 @@ def source_code_json_creation():
         "node.ast.label.l, node.ast.code.l, node.ast.lineNumber.l)))).toJsonPretty "
     )
     result = client.execute(query)
-    allData = json.loads(clean_json(result["stdout"]))
+    all_data = json.loads(clean_json(result["stdout"]))
 
     # For every field, create a dictionary and return it.
-    def create_field_dict(currField):
-        currFieldDict = {
-            "name": currField["_1"]["name"],
-            "modifiers": [x.lower() for x in currField["_2"]],
-            "type": currField["_1"]["typeFullName"],
+    def create_field_dict(curr_field):
+        curr_field_dict = {
+            "name": curr_field["_1"]["name"],
+            "modifiers": [x.lower() for x in curr_field["_2"]],
+            "type": curr_field["_1"]["typeFullName"],
         }
-        return currFieldDict
+        return curr_field_dict
 
     # For every method, create a dictionary and return it.
     def create_method_dict(curr_method):
         # For every method's instruction, create a dictionary and return it.
-        def create_instruction_dict(currLabel, currInstruction, currLineNumber):
+        def create_instruction_dict(curr_label, curr_instructions, curr_line_number):
             # Get the method call in each line of code, if any.
-            methodCallPattern = re.compile("([a-zA-Z]*\()")
-            calls = methodCallPattern.findall(currInstruction)
-            methodCall = ""
+            method_call_pattern = re.compile("([a-zA-Z]*\()")
+            calls = method_call_pattern.findall(curr_instructions)
+            method_call = ""
             if calls:
-                methodCall = calls[0].replace("(", "")
+                method_call = calls[0].replace("(", "")
 
-            currInstructionDict = {
-                "_label": currLabel,
-                "code": currInstruction.replace("\r\n", ""),
-                "lineNumber": currLineNumber or "none",
-                "methodCall": methodCall,
+            curr_instruction_dict = {
+                "_label": curr_label,
+                "code": curr_instructions.replace("\r\n", ""),
+                "lineNumber": curr_line_number or "none",
+                "methodCall": method_call,
             }
-            return currInstructionDict
+            return curr_instruction_dict
 
         if curr_method["_1"]["code"] == "<empty>":
             return
         else:
             # Get the modifiers, return type and the method body from the full method body provided by Joern.
-            modifiersPattern = re.compile("(private|public|protected|static|final)")
-            returnTypePattern = re.compile("(^[a-zA-z]*\s)")
-            methodNamePattern = re.compile("(^[a-zA-z]*)")
-            methodWithReturn = re.sub(
-                modifiersPattern, "", curr_method["_1"]["code"]
-            ).strip()
-            methodReturnType = returnTypePattern.findall(methodWithReturn)
-            returnType = ""
-            if methodReturnType:
-                returnType = methodReturnType[0].strip()
-            methodBody = re.sub(returnTypePattern, "", methodWithReturn)
-            constructorName = methodNamePattern.findall(methodBody)[0]
+            modifiers_pattern = re.compile("(private|public|protected|static|final|synchronized|volatile|abstract|native)")
+            return_type_pattern = re.compile("(^[a-zA-z]*\s)")
+            method_name_pattern = re.compile("(^[a-zA-z]*)")
 
-            currMethodDict = {
+            method_with_return = re.sub(
+                modifiers_pattern, "", curr_method["_1"]["code"]
+            ).strip()
+            method_return_type = return_type_pattern.findall(method_with_return)
+            return_type = ""
+
+            if method_return_type:
+                return_type = method_return_type[0].strip()
+            method_body = re.sub(return_type_pattern, "", method_with_return)
+            constructor_name = method_name_pattern.findall(method_body)[0]
+
+            curr_method_dict = {
                 "code": curr_method["_1"]["code"],
-                "methodBody": methodBody,
-                "modifiers": modifiersPattern.findall(curr_method["_1"]["code"]),
-                "name": curr_method["_1"]["name"].replace("<init>", constructorName),
+                "methodBody": method_body,
+                "modifiers": modifiers_pattern.findall(curr_method["_1"]["code"]),
+                "name": curr_method["_1"]["name"].replace("<init>", constructor_name),
                 # For the instructions,
                 # _2 corresponds to the labels,
                 # _3 corresponds to the code,
@@ -86,33 +88,33 @@ def source_code_json_creation():
                         curr_method["_4"],
                     )
                 ),
-                "returnType": returnType,
+                "returnType": return_type,
             }
-            return currMethodDict
+            return curr_method_dict
 
     # For every class, create a dictionary and return it.
-    def create_class_dict(currClass):
-        currClassDict = {
-            "name": currClass["_1"],
-            "fields": list(map(create_field_dict, currClass["_2"])),
+    def create_class_dict(curr_class):
+        curr_class_dict = {
+            "name": curr_class["_1"],
+            "fields": list(map(create_field_dict, curr_class["_2"])),
             "methods": list(
-                filter(None, list(map(create_method_dict, currClass["_3"])))
+                filter(None, list(map(create_method_dict, curr_class["_3"])))
             ),
         }
-        return currClassDict
+        return curr_class_dict
 
     # Create a dictionary with all of the info about the source code and write it to a .json file.
-    sourceCodeJSON = {"classes": list(map(create_class_dict, allData))}
-    joernResult = json.dumps(sourceCodeJSON, indent=4)
-    write_to_file(joernResult, "sourceCode.json")
+    source_code_json = {"classes": list(map(create_class_dict, all_data))}
+    joern_result = json.dumps(source_code_json, indent=4)
+    write_to_file(joern_result, "sourceCode.json")
 
 
 # Write the joern output of a query to a specified filename
-def write_to_file(joernResult, filename):
-    dirName = "src/main/python/joernFiles/"
-    finalName = dirName + filename
-    with open(finalName, "w") as f:
-        f.write(joernResult)
+def write_to_file(joern_result, file_name):
+    directory_name = "src/main/python/joernFiles/"
+    final_directory_name = directory_name + file_name
+    with open(final_directory_name, "w") as f:
+        f.write(joern_result)
 
 
 if __name__ == "__main__":
@@ -123,25 +125,28 @@ if __name__ == "__main__":
     # which will always store the imported source code
     directory = os.getcwd()
     directory = directory.replace("\\", "//")
-    dirName = directory + "/src/main/python/joernTestProject/src"
-    projectName = "analyzedProject"
+    directory_name = directory + "/src/main/python/joernTestProject/src"
+    project_name = "analyzedProject"
 
-    # For testing purposes. Get the path of src/main/java/com/CodeSmell as shown (replace '\\' with '/')
-    ourProjectDir = (
+    # For testing purposes. Full file paths are required for joern.
+    # Get the path of src/main/java/com/CodeSmell as shown (replace '\\' with '/')
+    our_project_dir = (
         "C:/Users/viski/OneDrive/Documents/GitHub/CodeSmell/src/main/java/com/CodeSmell"
     )
 
     # Original directory of where the source code we are analyzing came from. We could use the user's
     # original dir instead of taking .java source files, or just take in .java files as store in a dir
     # originalDir = "D:/SYSC3110/Lab1"
-    query = import_code_query(ourProjectDir, projectName)
+    query = import_code_query(our_project_dir, project_name)
     result = client.execute(query)
+    if result["success"]:
+        print("The source code has been successfully imported.")
 
     # Create the source code json representation
     source_code_json_creation()
 
     # Close and delete the project from user's bin/joern/joern-cli/workspace
-    query = 'delete ("' + projectName + '")'
+    query = 'delete ("' + project_name + '")'
     result = client.execute(query)
     if result["success"]:
         print("The source code has been successfully removed.")
