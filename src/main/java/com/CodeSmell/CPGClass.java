1b06302ad702e34e3cb2c4844159193b8fdf95fd
vikiru <viskirubakaran@cmail.carleton.ca>
2022-11-28 21:54:55 -0500

    Update CPGClass.java
    - add String values to Modifier
    - add parentClass, methodBody to Method
    - add filePath to CPGClass
    - added comments

--- src/main/java/com/CodeSmell/CPGClass.java
+++ src/main/java/com/CodeSmell/CPGClass.java
@@ -3,15 +3,27 @@ package com.CodeSmell;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+// A class within the source code
 public class CPGClass {
 
+    // the name of the class
     public final String name;
+
+    // the filePath of the class (full path)
+    public final String filePath;
+
+    // the type of the object (class, enum, abstract class, interface)
     public final String type;
+
+    // the list of methods within the class
     private ArrayList<Method> methods;
+
+    // the list of fields within the class
     private ArrayList<Attribute> attributes;
 
-    CPGClass(String name, String type) {
+    CPGClass(String name, String filePath, String type) {
         this.name = name;
+        this.filePath = filePath;
         this.type = type;
         this.methods = new ArrayList<Method>();
         this.attributes = new ArrayList<Attribute>();
@@ -34,35 +46,65 @@ public class CPGClass {
     }
 
     public enum Modifier {
-        PUBLIC,
-        PRIVATE,
-        PROTECTED,
-        STATIC,
-        SYNCHRONIZED,
-        VOLATILE,
-        ABSTRACT,
-        NATIVE,
-        FINAL
+        PUBLIC("public"),
+        PRIVATE("private"),
+        PROTECTED("protected"),
+        STATIC("protected"),
+        SYNCHRONIZED("synchronized"),
+        VOLATILE("volatile"),
+        ABSTRACT("abstract"),
+        NATIVE("native"),
+        FINAL("final");
+
+        public final String modString;
+
+        Modifier(String modString) {
+            this.modString = modString;
+        }
+
+        @Override
+        public String toString() {
+            return this.modString;
+        }
     }
 
+    // A method belonging to a class
     public static class Method {
+
+        // the parent class of the method (used to differentiate between methods within
+        // methodCalls)
+        public final CPGClass parentClass;
 
         // the name of the method
         public final String name;
+
+        // the method body of the method with parameters excluding the modifiers and
+        // return type i.e. "CPGClass(String name, String filePath, String type)"
+        public final String methodBody;
+
         // a print out of the method instructions
         public final Instruction[] instructions;
+
         // list of modifiers the method has (0 or more)
         public final Modifier[] modifiers;
+
         // a hashmap of all the method parameters, key is the name of the parameters
         // and value is the type
         public final HashMap<String, String> parameters;
+
+        // the return type of the method
         public final String returnType;
+
         // return a list of methods which this calls
         private ArrayList<Method> methodCalls;
 
+        protected Method(CPGClass parentClass, String name, String methodBody, Instruction[] instructions,
+                         Modifier[] modifiers,
+                         HashMap<String, String> parameters, String returnType) {
 
-        protected Method(String name, Instruction[] instructions, Modifier[] modifiers, HashMap<String, String> parameters, String returnType) {
+            this.parentClass = parentClass;
             this.name = name;
+            this.methodBody = methodBody;
             this.instructions = instructions;
             this.modifiers = modifiers;
             this.parameters = parameters;
@@ -70,11 +112,9 @@ public class CPGClass {
             this.methodCalls = new ArrayList<>();
         }
 
-
         protected void addMethodCall(Method m) {
             this.methodCalls.add(m);
         }
-
 
         public ArrayList<Method> getMethodCalls() {
             return methodCalls;
@@ -84,6 +124,12 @@ public class CPGClass {
             parameters.put(paramName, type);
         }
 
+        @Override
+        public String toString() {
+            return this.methodBody + ": " + this.returnType;
+        }
+
+        // The instructions (lines of code) within each method body
         public static class Instruction {
             // The label associated with each line of code (i.e. METHOD_RETURN, CALL, etc)
             public final String label;
@@ -98,8 +144,11 @@ public class CPGClass {
                 this.lineNumber = lineNumber;
             }
         }
+
+
     }
 
+    // An attribute belong to a class
     public static class Attribute {
         // the name of the attribute
         public final String name;
@@ -107,7 +156,7 @@ public class CPGClass {
         // list of modifiers the attribute has (0 or more)
         public final Modifier[] modifiers;
 
-        // the type of theattribute
+        // the type of the attribute
         public final String type;
 
         protected Attribute(String name, String type, Modifier[] modifiers) {
