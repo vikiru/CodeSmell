55185b8f15f7a6e42e516fbb87f49fc228102604
vikiru <viskirubakaran@cmail.carleton.ca>
2023-01-07 17:01:47 -0500

    Add some simple tests

---
+++ src/test/java/com/CodeSmell/MainAppTest.java
@@ -0,0 +1,9 @@
+package com.CodeSmell;
+
+import org.junit.Test;
+
+//todo use JavaFx, TestFX for this.
+public class MainAppTest
+{
+    @Test
+}
---
+++ src/test/java/com/CodeSmell/ParserTest.java
@@ -0,0 +1,27 @@
+package com.CodeSmell;
+
+import org.junit.Test;
+
+
+//todo - I'll do this once I finish Parser - V
+public class ParserTest {
+    @Test
+    public void testInitializeCPG() {
+    }
+
+    @Test
+    public void readFromJSON() {
+    }
+
+    @Test
+    public void testMultiplicities() {
+    }
+
+    @Test
+    public void testRelationships() {
+    }
+
+    @Test
+    public void verifyMethodCalls() {
+    }
+}
