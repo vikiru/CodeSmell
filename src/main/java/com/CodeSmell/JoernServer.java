d202895be5a83157d2a7da76183b42a44328df99
vikiru <viskirubakaran@cmail.carleton.ca>
2023-01-10 04:17:09 -0500

    Update JoernServer.java
    - rename joernServer

--- src/main/java/com/CodeSmell/JoernServer.java
+++ src/main/java/com/CodeSmell/JoernServer.java
@@ -6,7 +6,7 @@ import java.io.InputStreamReader;
 import java.nio.file.Paths;
 import java.io.File;
 
-public class joernServer {
+public class JoernServer {
     public static void main(String[] args) {
 
         // Get the path to joern
