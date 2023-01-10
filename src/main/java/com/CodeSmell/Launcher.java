dab176bbe2caccfbd1551686a2aaf2de7274326b
vikiru <viskirubakaran@cmail.carleton.ca>
2023-01-10 04:17:28 -0500

    Update Launcher.java
    - update launcher to reflect joernServer rename

--- src/main/java/com/CodeSmell/Launcher.java
+++ src/main/java/com/CodeSmell/Launcher.java
@@ -10,7 +10,7 @@ public class Launcher {
                 skipJoern = true;
             }
         }
-        if (!skipJoern) joernServer.main(args);
+        if (!skipJoern) JoernServer.main(args);
         MainApp.main(args);
     }
 }
