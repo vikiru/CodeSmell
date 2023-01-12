21b565d86afa03bb91b5f6ac36cf9bfb1242c9c1
vikiru <viskirubakaran@cmail.carleton.ca>
2023-01-12 18:31:07 -0500

    Update ParserTest.java
    - add write, read to/from json tests, relationship tests for association, composition, dependency, inheritance, realization.
    - added private fields and a @Before for reused objects like Parser, Gson, was going to do this for CPG as well.

--- src/test/java/com/CodeSmell/ParserTest.java
+++ src/test/java/com/CodeSmell/ParserTest.java
@@ -1,38 +1,231 @@
 package com.CodeSmell;
 
-import java.util.HashMap;
-
+import com.google.gson.Gson;
+import org.junit.Before;
 import org.junit.Test;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.fail;
-import com.CodeSmell.CPGClass;
-import com.CodeSmell.JoernServer;
 
-//todo - I'll do this once I finish Parser - V
+import java.io.File;
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.stream.Collectors;
+
+import static org.junit.Assert.*;
+
 public class ParserTest {
 
-    public class ClassA {
-        public final int[] arr;
-        ClassA(int[] arr) {
-            this.arr = arr;
-        }
+    private final String jsonPath = "src/main/python/joernFiles/sourceCode.json";
+    private Parser p;
+    private Gson gson;
 
-        public boolean compare(ClassA other) {
-            return other.arr == this.arr;
+    @Before
+    public void before() {
+        p = new Parser();
+        gson = new Gson();
+    }
+
+    @Test
+    public void readFromJSON() {
+        // Read the joern file using only gson, check to make sure classes are present and no relations are present
+        try (Reader reader = Files.newBufferedReader(Paths.get(jsonPath))) {
+            CodePropertyGraph withoutRelations = gson.fromJson(reader, CodePropertyGraph.class);
+            assertEquals("CPG has no relations within it.", false, withoutRelations.getRelations().size() > 0);
+            assertEquals("CPG has classes within it", true, withoutRelations.getClasses().size() > 0);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        // Read from the JSON obtained after Joern integration
+        CodePropertyGraph g = p.initializeCPG(jsonPath);
+        assertNotNull(g);
+        assertEquals("CPG has relations within it", true, g.getRelations().size() > 0);
+        assertEquals("CPG has classes within it", true, g.getClasses().size() > 0);
+
+        // Read from the JSON obtained after Parser writes to JSON file
+        CodePropertyGraph g2 = p.initializeCPG("src/main/python/joernFiles/sourceCodeWithRelations.json");
+        assertNotNull(g2);
+        assertEquals("CPG has relations within it", true, g2.getRelations().size() > 0);
+        assertEquals("CPG has classes within it", true, g2.getClasses().size() > 0);
+
+        assertEquals("Both CPGS have the same number of classes", g.getClasses().size(), g2.getClasses().size());
+        assertEquals("Both CPGS have the same number of relations", g.getRelations().size(), g2.getRelations().size());
+    }
+
+    @Test
+    public void writeToFile() {
+        String filePath = "src/main/python/joernFiles/sourceCodeEmpty.json";
+        // Write an empty graph to a file
+        CodePropertyGraph graph = new CodePropertyGraph();
+        p.writeToJson(graph, filePath);
+
+        // Read in the empty graph json file and ensure both graphs has 0 classes and 0 relations
+        CodePropertyGraph readGraph = p.initializeCPG(filePath);
+        assertEquals("Both CPGS have no classes", graph.getClasses().size(), readGraph.getClasses().size());
+        assertEquals("Both CPGS have no relations", graph.getRelations().size(), readGraph.getRelations().size());
+
+        // Delete the empty file
+        File fileToDelete = new File(filePath);
+        assertTrue(fileToDelete.delete());
+    }
+
+    @Test
+    public void testAssociationRelationship() {
+        String filePath = "src/main/python/joernFiles/sourceCodeWithRelations.json";
+        CodePropertyGraph cpg = p.initializeCPG(filePath);
+
+        var allAssociationRelationships = cpg.getRelations().stream().
+                filter(relation -> relation.type.equals(ClassRelation.Type.UNIDIRECTIONAL_ASSOCIATION) ||
+                        relation.type.equals(ClassRelation.Type.BIDIRECTIONAL_ASSOCIATION) ||
+                        relation.type.equals(ClassRelation.Type.REFLEXIVE_ASSOCIATION)).collect(Collectors.toList());
+
+        for (CodePropertyGraph.Relation r : allAssociationRelationships) {
+            CPGClass sourceClass = r.source;
+            CPGClass destClass = r.destination;
+            ClassRelation.Type type = r.type;
+
+            // collect all attributes of destClass where the attributeType matches sourceClass's name
+            // list should be empty for unidirectional and should not be empty for bidirectional association
+            var destAttributes = Arrays.stream(destClass.attributes).
+                    filter(attribute -> attribute.attributeType.equals(sourceClass.name)).collect(Collectors.toList());
+            boolean destClassHasNoSrcClassAttribute = destAttributes.isEmpty();
+
+            // reflexive association check
+            boolean sameClass = sourceClass == destClass;
+            assertEquals("Same class should have a reflexive association", sameClass,
+                    type.equals(ClassRelation.Type.REFLEXIVE_ASSOCIATION));
+
+            // unidirectional association check (1 way association, src -> dest)
+            assertEquals("Unidirectional relation is valid", true,
+                    destClassHasNoSrcClassAttribute &&
+                            type.equals(ClassRelation.Type.UNIDIRECTIONAL_ASSOCIATION));
+
+            assertEquals("Unidirectional relation should not be valid", false,
+                    !destClassHasNoSrcClassAttribute &&
+                            type.equals(ClassRelation.Type.UNIDIRECTIONAL_ASSOCIATION));
+
+            // bidirectional association check (2 way association, src -> dest)
+            assertEquals("Bidirectionl association is valid", true,
+                    !destClassHasNoSrcClassAttribute && type.equals(ClassRelation.Type.BIDIRECTIONAL_ASSOCIATION));
+
+            assertEquals("Bidirectional association should not be valid", false,
+                    destClassHasNoSrcClassAttribute && type.equals(ClassRelation.Type.BIDIRECTIONAL_ASSOCIATION));
+
+            assertEquals("Multiplicity should not be empty", false, r.multiplicity.equals(""));
         }
     }
 
     @Test
+    public void testCompositionRelationship() {
+        String filePath = "src/main/python/joernFiles/sourceCodeWithRelations.json";
+        CodePropertyGraph cpg = p.initializeCPG(filePath);
+        var allCompositonRelationships = cpg.getRelations().stream().
+                filter(relation -> relation.type.equals(ClassRelation.Type.COMPOSITION)).collect(Collectors.toList());
+        for (CodePropertyGraph.Relation r : allCompositonRelationships) {
+            CPGClass sourceClass = r.source;
+            CPGClass destClass = r.destination;
+            var attributesMatchingDest = Arrays.stream(sourceClass.attributes).
+                    filter(attribute -> attribute.attributeType.equals(destClass.name)).collect(Collectors.toList());
+            var constructorResult = Arrays.stream(sourceClass.methods).
+                    filter(method -> method.name.equals(sourceClass.name)).collect(Collectors.toList());
+
+            if (!constructorResult.isEmpty()) {
+                CPGClass.Method constructor = constructorResult.get(0);
+                assertEquals("Dest class should not be within constructor params",
+                        true, !constructor.methodBody.contains(destClass.name));
+                var constructorInstructions = Arrays.stream(constructor.instructions).filter(instruction ->
+                        instruction.code.contains(destClass.name)).collect(Collectors.toList());
+                assertEquals("Constructor instructions should contain dest class", true,
+                        !constructorInstructions.isEmpty());
+            } else {
+                for (CPGClass.Attribute a : attributesMatchingDest) {
+                    assertEquals("Attribute should not be static", true, !a.code.contains("static"));
+                }
+            }
+        }
+    }
+
+    @Test
+    public void testDependencyRelationship() {
+        String filePath = "src/main/python/joernFiles/sourceCodeWithRelations.json";
+        CodePropertyGraph cpg = p.initializeCPG(filePath);
+        var allDependencyRelationships = cpg.getRelations().stream().
+                filter(relation -> relation.type.equals(ClassRelation.Type.INHERITANCE)).collect(Collectors.toList());
+
+        for (CodePropertyGraph.Relation r : allDependencyRelationships) {
+            CPGClass sourceClass = r.source;
+            CPGClass destinationClass = r.destination;
+            var sourceClassMethods = Arrays.stream(sourceClass.methods).
+                    filter(method -> !method.name.equals(sourceClass.name)).collect(Collectors.toList());
+            var sourceClassAttributes = Arrays.stream(sourceClass.attributes).
+                    filter(attribute -> attribute.attributeType.equals(destinationClass.name)).collect(Collectors.toList());
+            assertEquals("Source class should not have attributes matching destination class",
+                    true, sourceClassAttributes.isEmpty());
+
+            for (CPGClass.Method m : sourceClassMethods) {
+                var methodCalls = m.getMethodCalls().stream().
+                        filter(method -> method.parentClassName.equals(destinationClass)).collect(Collectors.toList());
+                boolean destClassWithinMethodBody = m.methodBody.contains(destinationClass.name);
+                boolean destClassWithinMethodCalls = methodCalls.isEmpty();
+                assertEquals("Destination class should be within method params or method calls", true,
+                        destClassWithinMethodBody || destClassWithinMethodCalls);
+            }
+        }
+
+    }
+
+    @Test
+    public void testInheritanceRelationship() {
+        String filePath = "src/main/python/joernFiles/sourceCodeWithRelations.json";
+        CodePropertyGraph cpg = p.initializeCPG(filePath);
+        var allInheritanceRelationships = cpg.getRelations().stream().
+                filter(relation -> relation.type.equals(ClassRelation.Type.INHERITANCE)).collect(Collectors.toList());
+
+        for (CodePropertyGraph.Relation r : allInheritanceRelationships) {
+            CPGClass subClass = r.source;
+            CPGClass superClass = r.destination;
+            assertEquals("Subclass extends superclass", true,
+                    subClass.code.contains("extends") && subClass.code.contains(superClass.name));
+            assertEquals("Superclass does not extend subclass", true, !superClass.code.contains(subClass.name));
+            assertEquals("Multiplicity should be left blank", "", r.multiplicity);
+        }
+    }
+
+    @Test
+    public void testRealizationRelationship() {
+        String filePath = "src/main/python/joernFiles/sourceCodeWithRelations.json";
+        CodePropertyGraph cpg = p.initializeCPG(filePath);
+        var allRealizationRelationships = cpg.getRelations().stream().
+                filter(relation -> relation.type.equals(ClassRelation.Type.REALIZATION)).collect(Collectors.toList());
+
+        for (CodePropertyGraph.Relation r : allRealizationRelationships) {
+            CPGClass source = r.source;
+            CPGClass destination = r.destination;
+            assertEquals("Source class should be a class or abstract class",
+                    true, source.classType.contains("class"));
+            assertEquals("Destination class should be an interface", true,
+                    destination.classType.contains("interface"));
+
+            // can implement multiple interfaces, but extend only one
+            assertEquals("Source class should contain implements and destination class within declaration", true,
+                    source.code.contains("implements") && source.code.contains(destination.name));
+            assertEquals("Destination class should not implement", true, !destination.code.contains("implements"));
+        }
+    }
+
+
+    // ----------------------------------------------------------------------------------------------------------
+
+    @Test
     public void testInitializeCPG() {
-        Parser p = new Parser();
         JoernServer.start(true);
-        CodePropertyGraph g = p.initializeCPG("src/main/python/joernFiles/sourceCode.json");
+        CodePropertyGraph g = p.initializeCPG(jsonPath);
         //assertEquals(g.getRelations().size(), g.getClasses().size(), 0);
         //int[] arr = new int[] {1, 2, 3};
         //ClassA a = new ClassA(arr);
         //ClassA a2 = new ClassA(arr);
-       // System.out.println(a.compare(a2));
+        // System.out.println(a.compare(a2));
 
         for (CodePropertyGraph.Relation r : g.getRelations()) {
             System.out.println("----------");
@@ -43,52 +236,42 @@ public class ParserTest {
 
     @Test
     public void testOwnProject() {
-        Parser p = new Parser();
         JoernServer.start(false);
-        CodePropertyGraph g = p.initializeCPG("src/main/python/joernFiles/sourceCode.json");
-        HashMap<CPGClass, Boolean> connectedClasses = new HashMap<CPGClass, Boolean>(); 
-         for (CPGClass c : g.getClasses()) {
+        CodePropertyGraph g = p.initializeCPG(jsonPath);
+        HashMap<CPGClass, Boolean> connectedClasses = new HashMap<CPGClass, Boolean>();
+        for (CPGClass c : g.getClasses()) {
             connectedClasses.put(c, false);
         }
 
-
         for (CodePropertyGraph.Relation r : g.getRelations()) {
-            boolean sourceCheck = connectedClasses.get(r.source); 
+            boolean sourceCheck = connectedClasses.get(r.source);
             boolean destCheck = connectedClasses.get(r.destination);
             // ensure the classes in the relation are the
             // ones in the classes list
             String s = "Class in relation not found in cpg class list\n";
-            assertNotNull( s + r.source, sourceCheck);
+            assertNotNull(s + r.source, sourceCheck);
             assertNotNull(s + r.destination, destCheck);
 
             connectedClasses.put(r.source, true);
             connectedClasses.put(r.destination, true);
         }
 
-        connectedClasses.forEach( (c, isConnected) -> {
+        connectedClasses.forEach((c, isConnected) -> {
             assertEquals("Disconnected class: " + c, isConnected, true);
         });
     }
 
-    @Test
-    public void testBuildCPG() {
-        //CodePropertyGraph g = Parser.buildCPG("src/main/python/joernFiles/sourceCode.json");
-        //assertEquals(g.getRelations().size(), g.getClasses().size(), 1);
+    public class ClassA {
+        public final int[] arr;
+
+        ClassA(int[] arr) {
+            this.arr = arr;
+        }
+
+        public boolean compare(ClassA other) {
+            return other.arr == this.arr;
+        }
     }
 
-    @Test
-    public void readFromJSON() {
-    }
 
-    @Test
-    public void testMultiplicities() {
-    }
-
-    @Test
-    public void testRelationships() {
-    }
-
-    @Test
-    public void verifyMethodCalls() {
-    }
 }
