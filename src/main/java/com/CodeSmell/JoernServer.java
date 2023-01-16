04204c77d17a5c1f8f727b70620f3cca71c90bab
vikiru <viskirubakaran@cmail.carleton.ca>
2023-01-16 18:07:44 -0500

    Update JoernServer.java
    - remove blocking behaviour by removing waitFor() call to joern_query and also trying to read in the joern_query error stream

--- src/main/java/com/CodeSmell/JoernServer.java
+++ src/main/java/com/CodeSmell/JoernServer.java
@@ -1,15 +1,7 @@
 package com.CodeSmell;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.nio.file.Paths;
-
-import java.io.File;
-import java.io.ByteArrayInputStream;
-import java.nio.charset.StandardCharsets;
 
 public class JoernServer {
 
@@ -20,12 +12,11 @@ public class JoernServer {
         return this.joernStream;
     }
 
-
     public void start(String directory) {
-
-        System.out.println("Starting joern in " + directory);
         // Get the path to joern
         String joernPath = System.getProperty("user.home") + "/bin/joern/joern-cli";
+
+        System.out.println("JoernServer :: Starting Joern in directory: " + directory);
         String directoryPath = Paths.get("").toAbsolutePath() + "/src/main/python";
 
         // Start up a command prompt terminal (no popup) and start the joern server
@@ -43,56 +34,39 @@ public class JoernServer {
             // Start the server
             Process joernServerProcess = joernServerBuilder.start();
             BufferedReader joernServerReader = new BufferedReader(
-                new InputStreamReader(joernServerProcess.getInputStream()));
+                    new InputStreamReader(joernServerProcess.getInputStream()));
             BufferedReader errorReader = new BufferedReader(
-                new InputStreamReader(joernServerProcess.getErrorStream()));;
+                    new InputStreamReader(joernServerProcess.getErrorStream()));
+
             String line;
             while ((line = joernServerReader.readLine()) != null) {
-                System.out.println(joernServerReader.readLine());
+                System.out.println("JoernServer Input Stream Output :: " + joernServerReader.readLine());
             }
             while ((line = errorReader.readLine()) != null) {
-                System.out.println(line);
+                System.out.println("JoernServer Error Stream Output :: " + line);
             }
 
             // Execute queries against the local joern server instance.
             Process joernQueryProcess = joernQueryBuilder.start();
-
             this.joernStream = joernQueryProcess.getInputStream();
-            errorReader = new BufferedReader(new InputStreamReader(joernQueryProcess.getErrorStream()));
-            int exitCode = joernQueryProcess.waitFor();
-            System.out.println("Joern exit code: " + exitCode);
-            while ((line = errorReader.readLine()) != null) {
-                System.out.println(line);
+
+            // Reading in joernQuery Error Stream and using waitFor() on joernQuery process causes extreme delays / blocking.
+
+            // For Windows OS, find the process bound to port 8080 and kill it.
+            if (System.getProperty("os.name").contains("Windows")) {
+                Process findProcess = windowsPortFinderBuilder.start();
+                BufferedReader findProcessReader = new BufferedReader(new InputStreamReader(findProcess.getInputStream()));
+
+                // Obtain the PID of the process bound to port 8080
+                String lineFree = findProcessReader.readLine();
+                String processID = lineFree.substring(lineFree.lastIndexOf(" ")).trim();
+
+                // Kill the process bound to port 8080
+                ProcessBuilder portFreerBuilder = new ProcessBuilder("cmd.exe", "/c", "taskkill /F /PID " + processID);
+                Process freeProcess = portFreerBuilder.start();
             }
-            // Wait for joern_query to finish and then cleanup by killing the process which started the
-            // local server instance.
-            if (exitCode == 0) {
-                joernServerProcess.destroy();
-                if (joernServerProcess.isAlive()) {
-                    joernServerProcess.destroyForcibly();
-                }
-
-                // For Windows OS, find the process bound to port 8080 and kill it.
-                // todo - add commands for other OS such as linux.
-                if (System.getProperty("os.name").contains("Windows")) {
-                    Process findProcess = windowsPortFinderBuilder.start();
-                    BufferedReader findProcessReader = new BufferedReader(new InputStreamReader(findProcess.getInputStream()));
-
-                    // Obtain the PID of the process bound to port 8080
-                    String lineFree = findProcessReader.readLine();
-                    String processID = lineFree.substring(lineFree.lastIndexOf(" ")).trim();
-
-                    // Kill the process bound to port 8080
-                    ProcessBuilder portFreerBuilder = new ProcessBuilder("cmd.exe", "/c", "taskkill /F /PID " + processID);
-                    Process freeProcess = portFreerBuilder.start();
-                }
-                return;
-            }
-            throw new RuntimeException("Joern server error");
         } catch (IOException e) {
             e.printStackTrace();
-            throw new RuntimeException(e);
-        } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
     }
