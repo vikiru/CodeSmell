9d5c5b01bc438fd40fd8a64e65fc948af5b40a8f
vikiru <viskirubakaran@cmail.carleton.ca>
2023-02-01 16:12:47 -0500

    Update module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,12 +6,14 @@ module jfx {
     requires java.desktop;
     requires com.google.gson;
 
-    opens com.CodeSmell to javafx.fxml, com.google.gson;
     exports com.CodeSmell;
-    exports com.CodeSmell.parser;
-    opens com.CodeSmell.parser to com.google.gson, javafx.fxml;
     exports com.CodeSmell.model;
     opens com.CodeSmell.model to com.google.gson, javafx.fxml;
+    opens com.CodeSmell to javafx.fxml, com.google.gson;
+    exports com.CodeSmell.parser;
+    opens com.CodeSmell.parser to com.google.gson, javafx.fxml;
+    exports com.CodeSmell.smell;
+    opens com.CodeSmell.smell to com.google.gson, javafx.fxml;
     exports com.CodeSmell.view;
     opens com.CodeSmell.view to com.google.gson, javafx.fxml;
 }
