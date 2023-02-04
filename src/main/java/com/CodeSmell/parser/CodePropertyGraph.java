e42e11f1033b6b794fcedc7cd3b63b5b87bc62d0
vikiru <viskirubakaran@cmail.carleton.ca>
2023-02-03 21:30:31 -0500

    Update CodePropertyGraph.java
    - update comments

--- src/main/java/com/CodeSmell/parser/CodePropertyGraph.java
+++ src/main/java/com/CodeSmell/parser/CodePropertyGraph.java
@@ -5,9 +5,19 @@ import com.CodeSmell.model.ClassRelation.RelationshipType;
 import java.io.Serializable;
 import java.util.ArrayList;
 
+/**
+ * The CodePropertyGraph which contains all the classes and relations
+ */
 public class CodePropertyGraph implements Serializable {
 
+    /**
+     * All the relationships between classes within the CodePropertyGraph
+     */
     private ArrayList<Relation> relations;
+
+    /**
+     * All the classes within the CodePropertyGraph
+     */
     private ArrayList<CPGClass> classes;
 
     protected CodePropertyGraph() {
@@ -31,18 +41,46 @@ public class CodePropertyGraph implements Serializable {
         return new ArrayList<>(this.relations);
     }
 
+    /**
+     * Add a {@link CPGClass} to the CodePropertyGraph
+     *
+     * @param c - The class to be added
+     */
     protected void addClass(CPGClass c) {
         this.classes.add(c);
     }
 
+    /**
+     * Add a {@link Relation} to the CodePropertyGraph
+     *
+     * @param r - The relation to be added
+     */
     protected void addRelation(Relation r) {
         this.relations.add(r);
     }
 
+    /**
+     * A relationship that exists between two classes, with a type and associated multiplicity (if any)
+     */
     public static class Relation implements Serializable {
+        /**
+         * The source class of the relationship
+         */
         public final CPGClass source;
+
+        /**
+         * The destination class of the relationship
+         */
         public final CPGClass destination;
+
+        /**
+         * The {@link RelationshipType} that defines this relation (e.g. COMPOSITION)
+         */
         public final RelationshipType type;
+
+        /**
+         * The multiplicity associated with the relation (empty for relations other than some form of ASSOCIATION relation)
+         */
         public final String multiplicity;
 
         Relation(CPGClass source, CPGClass destination, RelationshipType type, String multiplicity) {
