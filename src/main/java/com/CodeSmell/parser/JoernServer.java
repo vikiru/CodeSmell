6ec9f78274e720fe8b627750f0d210a819e57513
vikiru <viskirubakaran@cmail.carleton.ca>
2023-02-04 15:49:15 -0500

    Update JoernServer.java
    - fix joern server random port for windows
    - add full directory path for joern so that the final close and delete query in joern_query works and joern-query properly exits now since previously the close and delete query did not return due to not having full path

--- src/main/java/com/CodeSmell/parser/JoernServer.java
+++ src/main/java/com/CodeSmell/parser/JoernServer.java
@@ -19,116 +19,119 @@ import com.CodeSmell.parser.Parser;
 public class JoernServer {
 
 
-	private static Random rand = new Random();
-	InputStream joernStream;
+    private static Random rand = new Random();
+    InputStream joernStream;
 
-	public static int nextFreePort(int from, int to) {
-	// https://stackoverflow.com/questions/2675362/how-to-find-an-available-port
-    int port = rand.nextInt(to - from) + from;
-    while (true) {
-        if (isLocalPortFree(port)) {
-            return port;
-        } else {
-            port = rand.nextInt(to - from) + from;
+    public static int nextFreePort(int from, int to) {
+        // https://stackoverflow.com/questions/2675362/how-to-find-an-available-port
+        int port = rand.nextInt(to - from) + from;
+        while (true) {
+            if (isLocalPortFree(port)) {
+                return port;
+            } else {
+                port = rand.nextInt(to - from) + from;
+            }
         }
     }
-}
 
-	private static boolean isLocalPortFree(int port) {
-		// https://stackoverflow.com/questions/2675362/how-to-find-an-available-port
-	    try {
-	        new ServerSocket(port).close();
-	        return true;
-	    } catch (IOException e) {
-	        return false;
-	    }
-	}
+    private static boolean isLocalPortFree(int port) {
+        // https://stackoverflow.com/questions/2675362/how-to-find-an-available-port
+        try {
+            new ServerSocket(port).close();
+            return true;
+        } catch (IOException e) {
+            return false;
+        }
+    }
 
-	public static class ReaderThread extends Thread {
+    public static class ReaderThread extends Thread {
 
-		private BufferedReader reader; 
+        private BufferedReader reader;
 
-		public ReaderThread(BufferedReader reader) {
-			this.reader = reader;
-		}
+        public ReaderThread(BufferedReader reader) {
+            this.reader = reader;
+        }
 
-		public void run() {
-			while (true) {
-				try {
-					String line;
-					while ((line = this.reader.readLine()) != null) {
-					   System.out.println(line);
-					}
-				} catch (IOException e) {
-					e.printStackTrace();
-					System.exit(1);
-				}
-			}
-		}
-	}
+        public void run() {
+            while (true) {
+                try {
+                    String line;
+                    while ((line = this.reader.readLine()) != null) {
+                        System.out.println(line);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    System.exit(1);
+                }
+            }
+        }
+    }
 
-	public InputStream getStream() {
-		return this.joernStream;
-	}
+    public InputStream getStream() {
+        return this.joernStream;
+    }
 
-	public void start(File directory) {
-		if (!directory.isDirectory()) {
-			throw new RuntimeException("JoernServer got bad directory: " + directory);
-		}
+    public void start(File directory) {
+        if (!directory.isDirectory()) {
+            throw new RuntimeException("JoernServer got bad directory: " + directory);
+        }
 
-		// Get the path to joern
-		String joernPath = System.getProperty("user.home") + "/bin/joern/joern-cli";
+        // Get the path to joern
+        String joernPath = System.getProperty("user.home") + "/bin/joern/joern-cli";
 
-		System.out.println("Starting Joern in directory: " + directory);
-		String directoryPath = Paths.get("").toAbsolutePath() + "/src/main/python";
+        String rootDir = String.valueOf(Paths.get("").toAbsolutePath());
+        String dirString = rootDir + File.separator + directory.toString();
+        System.out.println("Starting Joern in directory: " + dirString);
+        String directoryPath = rootDir + "/src/main/python";
 
-		// Start up a command prompt terminal (no popup) and start the joern server
-		ProcessBuilder joernServerBuilder, joernQueryBuilder, windowsPortFinderBuilder;
+        // Start up a command prompt terminal (no popup) and start the joern server
+        ProcessBuilder joernServerBuilder, joernQueryBuilder, windowsPortFinderBuilder;
 
-		int serverPort = nextFreePort(8000, 9999);
+        int serverPort = nextFreePort(8000, 9999);
 
-		// Open terminal and start the joern server once process for joernServerBuilder starts
-		windowsPortFinderBuilder = new ProcessBuilder("cmd.exe", "/c", "netstat -ano | findstr 8080");
-		if (System.getProperty("os.name").contains("Windows")) {
-			joernServerBuilder = new ProcessBuilder("cmd.exe", "/c", "joern", "--server");
-		} else {
-			joernServerBuilder = new ProcessBuilder("joern", "--server-host",  "localhost",
-				"--server-port", String.valueOf(serverPort), "--server");
-		}
-		joernQueryBuilder = new ProcessBuilder("python", "joern_query.py", 
-			directory.toString(), String.valueOf(serverPort)).directory(new File(directoryPath));
+        // Open terminal and start the joern server once process for joernServerBuilder starts
+        windowsPortFinderBuilder = new ProcessBuilder("cmd.exe", "/c", "netstat -ano | findstr 8080");
+        if (System.getProperty("os.name").contains("Windows")) {
+            joernServerBuilder = new ProcessBuilder("cmd.exe", "/c", "joern", "--server",
+                    "--server-host", "localhost",
+                    "--server-port", String.valueOf(serverPort));
+        } else {
+            joernServerBuilder = new ProcessBuilder("joern", "--server-host", "localhost",
+                    "--server-port", String.valueOf(serverPort), "--server");
+        }
+        joernQueryBuilder = new ProcessBuilder("python", "joern_query.py",
+                dirString, String.valueOf(serverPort)).directory(new File(directoryPath));
 
+        try {
+            // Start the server
+            Process joernServerProcess = joernServerBuilder.start();
+            BufferedReader joernServerReader = new BufferedReader(
+                    new InputStreamReader(joernServerProcess.getInputStream()));
+            BufferedReader errorReader = new BufferedReader(
+                    new InputStreamReader(joernServerProcess.getErrorStream()));
+            new ReaderThread(joernServerReader).start();
+            new ReaderThread(errorReader).start();
+            // Execute queries against the local joern server instance.
+            Process joernQueryProcess = joernQueryBuilder.start();
 
-		try {
-			// Start the server
-			Process joernServerProcess = joernServerBuilder.start();
-			BufferedReader joernServerReader = new BufferedReader(
-					new InputStreamReader(joernServerProcess.getInputStream()));
-			BufferedReader errorReader = new BufferedReader(
-					new InputStreamReader(joernServerProcess.getErrorStream()));
-			new ReaderThread(joernServerReader).start();
-			new ReaderThread(errorReader).start();
-			// Execute queries against the local joern server instance.
-			Process joernQueryProcess = joernQueryBuilder.start();
+            // log joern_query.py standard error output
+            new ReaderThread(
+                    new BufferedReader(
+                            new InputStreamReader(
+                                    joernQueryProcess.getErrorStream()))).start();
 
-			// log joern_query.py standard error output
-			new ReaderThread(
-				new BufferedReader(
-				new InputStreamReader(
-				joernQueryProcess.getErrorStream()))).start();
+            // log joern_query.py loggin.info() output
+            new ReaderThread(
+                    new BufferedReader(
+                            new FileReader(
+                                    Parser.JOERN_QUERY_LOGFILE))).start();
 
-			// log joern_query.py loggin.info() output
-			new ReaderThread(
-				new BufferedReader (
-				new FileReader(
-				Parser.JOERN_QUERY_LOGFILE))).start();
+            this.joernStream = joernQueryProcess.getInputStream();
+            System.out.println("Exiting JoernServer callstack");
 
-			this.joernStream = joernQueryProcess.getInputStream();
-			System.out.println("Exiting JoernServer callstack");
-
-		} catch (IOException e) {
-			e.printStackTrace();
-			throw new RuntimeException(e);
-		}
-	}
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+    }
 }
