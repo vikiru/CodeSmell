388c34938cb8699dedfb5206c72673147d69a946
vikiru <viskirubakaran@cmail.carleton.ca>
2023-02-07 00:50:59 -0500

    Fix package import so no errors are thrown

--- src/test/java/com/testproject/ClassD.java
+++ src/test/java/com/testproject/ClassD.java
@@ -1,22 +1,21 @@
-package com.CodeSmell.testproject;
+package com.testproject;
 
-import com.CodeSmell.testproject.ClassB;
 
 public class ClassD implements InterfaceC {
-	private ClassB b;
-	private int counter;
+    private ClassB b;
+    private int counter;
 
-	ClassD(ClassB b) {
-		this.b = b;
-	}
+    ClassD(ClassB b) {
+        this.b = b;
+    }
 
-	public void doThing() {
-		b.i += 1;
-		counter = b.i * 3;
-	}
+    public void doThing() {
+        b.i += 1;
+        counter = b.i * 3;
+    }
 
-	public int getCounter() {
-		return this.counter;
-	}
+    public int getCounter() {
+        return this.counter;
+    }
 
 }
