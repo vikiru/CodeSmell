758151d645338971f97f251527bb2254f88051f7
vikiru <viskirubakaran@cmail.carleton.ca>
2023-02-14 01:39:21 -0500

    Update Helper.java
    - cleanup
    - switch from void to return field types
    - switch all to unmodifiable collections

--- src/main/java/com/CodeSmell/stat/Helper.java
+++ src/main/java/com/CodeSmell/stat/Helper.java
@@ -4,7 +4,9 @@ import com.CodeSmell.parser.CPGClass;
 import com.CodeSmell.parser.CodePropertyGraph;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashSet;
+import java.util.List;
 
 /**
  * A class that contains potentially helpful lists containing info such as all attributes, all methods,
@@ -15,144 +17,144 @@ public final class Helper {
     /**
      * All the {@link com.CodeSmell.parser.CPGClass.Attribute} that exist within the cpg
      */
-    public final ArrayList<CPGClass.Attribute> allAttributes = new ArrayList<>();
-
-    /**
-     * All the {@link com.CodeSmell.parser.CPGClass.Method} that exist within the cpg
-     */
-    public final ArrayList<CPGClass.Method> allMethods = new ArrayList<>();
-
-    /**
-     * All the {@link com.CodeSmell.parser.CPGClass.Method} calls that exist within the cpg
-     */
-    public final ArrayList<CPGClass.Method> allMethodCalls = new ArrayList<>();
-
-    /**
-     * All the {@link com.CodeSmell.parser.CPGClass.Attribute} calls that exist within the cpg
-     */
-    public final ArrayList<CPGClass.Attribute> allAttributeCalls = new ArrayList<>();
-
-    /**
-     * All the {@link com.CodeSmell.parser.CPGClass.Method.Parameter} that exist within the cpg
-     */
-    public final ArrayList<CPGClass.Method.Parameter> allParameters = new ArrayList<>();
-
-    /**
-     * All the class names (both name and classFullName) that exist within the cpg
-     */
-    public final ArrayList<String> allClassNames = new ArrayList<>();
-
-    /**
-     * All the method names that exist within the cpg
-     */
-    public final ArrayList<String> allMethodNames = new ArrayList<>();
-
+    public final List<CPGClass.Attribute> allAttributes;
     /**
      * All the attribute names that exist within the cpg
      */
-    public final ArrayList<String> allAttributeNames = new ArrayList<>();
+    public final List<String> allAttributeNames;
+    /**
+     * All the class names (both name and classFullName) that exist within the cpg
+     */
+    public final List<String> allClassNames;
+    /**
+     * All the {@link com.CodeSmell.parser.CPGClass.Method} that exist within the cpg
+     */
+    public final List<CPGClass.Method> allMethods;
+    /**
+     * All the method names that exist within the cpg
+     */
+    public final List<String> allMethodNames;
+    /**
+     * All the {@link com.CodeSmell.parser.CPGClass.Method.Parameter} that exist within the cpg
+     */
+    public final List<CPGClass.Method.Parameter> allParameters;
+    /**
+     * All the {@link com.CodeSmell.parser.CPGClass.Attribute} calls that exist within the cpg
+     */
+    public final List<CPGClass.Attribute> allAttributeCalls;
+    /**
+     * All the {@link com.CodeSmell.parser.CPGClass.Method} calls that exist within the cpg
+     */
+    public final List<CPGClass.Method> allMethodCalls;
+
 
     public Helper(CodePropertyGraph cpg) {
-        collectAllAttributes(cpg, allAttributes);
-        collectAllAttributeNames(cpg, allAttributeNames);
-        collectAllMethods(cpg, allMethods);
-        collectAllMethodNames(cpg, allMethodNames);
-        collectAllParameters(allMethods, allParameters);
-        collectAllMethodCalls(allMethods, allMethodCalls);
-        collectAllAttributeCalls(allMethods, allAttributeCalls);
-        collectAllClassNames(cpg, allClassNames);
+        this.allAttributes = collectAllAttributes(cpg);
+        this.allAttributeNames = collectAllAttributeNames(cpg);
+        this.allClassNames = collectAllClassNames(cpg);
+        this.allMethods = collectAllMethods(cpg);
+        this.allMethodNames = collectAllMethodNames(cpg);
+        this.allParameters = collectAllParameters(allMethods);
+        this.allAttributeCalls = collectAllAttributeCalls(allMethods);
+        this.allMethodCalls = collectAllMethodCalls(allMethods);
     }
 
     /**
      * Collect all the attributes that exist within cpg
      *
-     * @param cpg           The CodePropertyGraph containing all existing classes and relations
-     * @param allAttributes A list containing all the attributes within cpg
+     * @param cpg The CodePropertyGraph containing all existing classes and relations
      */
-    private static void collectAllAttributes(CodePropertyGraph cpg,
-                                             ArrayList<CPGClass.Attribute> allAttributes) {
-        cpg.getClasses().forEach(cpgClass -> allAttributes.addAll(cpgClass.getAttributes()));
+    private static List<CPGClass.Attribute> collectAllAttributes(CodePropertyGraph cpg) {
+        List<CPGClass.Attribute> allAttributes = new ArrayList<>();
+        cpg.getClasses().
+                forEach(cpgClass -> allAttributes.addAll(cpgClass.getAttributes()));
+        return Collections.unmodifiableList(allAttributes);
     }
 
     /**
      * Collect all the attribute names that exist within cpg
      *
-     * @param cpg               The CodePropertyGraph containing all existing classes and relations
-     * @param allAttributeNames A list containing all the attribute names within cpg
+     * @param cpg The CodePropertyGraph containing all existing classes and relations
      */
-    private static void collectAllAttributeNames(CodePropertyGraph cpg, ArrayList<String> allAttributeNames) {
-        cpg.getClasses().forEach(cpgClass -> cpgClass.getAttributes().forEach(attribute -> allAttributeNames.add(attribute.name)));
-    }
-
-    /**
-     * Collect all the methods that exist within cpg.
-     *
-     * @param cpg        The CodePropertyGraph containing all existing classes and relations
-     * @param allMethods A list containing all the methods that exist within cpg
-     */
-    private static void collectAllMethods(CodePropertyGraph cpg,
-                                          ArrayList<CPGClass.Method> allMethods) {
-        cpg.getClasses().forEach(cpgClass -> allMethods.addAll(cpgClass.getMethods()));
-    }
-
-    /**
-     * Collect all the method names that exist within the cpg into one single list.
-     *
-     * @param cpg            The CodePropertyGraph containing all existing classes and relations
-     * @param allMethodNames A list containing every method name in cpg
-     */
-    private static void collectAllMethodNames(CodePropertyGraph cpg,
-                                              ArrayList<String> allMethodNames) {
-        cpg.getClasses().forEach(cpgClass -> cpgClass.getMethods().forEach(method -> allMethodNames.add(method.name)));
-    }
-
-
-    /**
-     * Collect all the method calls of each method into one single list.
-     *
-     * @param allMethods     All the methods within cpg
-     * @param allMethodCalls A list containing all the method calls across all methods
-     */
-    private static void collectAllMethodCalls(ArrayList<CPGClass.Method> allMethods,
-                                              ArrayList<CPGClass.Method> allMethodCalls) {
-        allMethods.forEach(method -> allMethodCalls.addAll(method.getMethodCalls()));
-    }
-
-    /**
-     * Collect all the attribute calls of each method into one single list.
-     *
-     * @param allMethods        All the methods within cpg
-     * @param allAttributeCalls A list containing all the attribute calls across all methods
-     */
-    private static void collectAllAttributeCalls(ArrayList<CPGClass.Method> allMethods,
-                                                 ArrayList<CPGClass.Attribute> allAttributeCalls) {
-        allMethods.forEach(method -> allAttributeCalls.addAll(method.getAttributeCalls()));
-    }
-
-    /**
-     * Collect all the method parameters of each method into one single list.
-     *
-     * @param allMethods    All the methods within cpg
-     * @param allParameters A list containing all the method parameters across all methods
-     */
-    private static void collectAllParameters(ArrayList<CPGClass.Method> allMethods,
-                                             ArrayList<CPGClass.Method.Parameter> allParameters) {
-        allMethods.forEach(method -> allParameters.addAll((method.parameters)));
+    private static List<String> collectAllAttributeNames(CodePropertyGraph cpg) {
+        List<String> allAttributeNames = new ArrayList<>();
+        cpg.getClasses().
+                forEach(cpgClass -> cpgClass.getAttributes().
+                        forEach(attribute -> allAttributeNames.add(attribute.name)));
+        return Collections.unmodifiableList(allAttributeNames);
     }
 
     /**
      * Collect all the class names that exist within the cpg (both name and classFullName) into one single list.
      *
-     * @param cpg           The CodePropertyGraph containing all existing classes and relations
-     * @param allClassNames A list containing the names of every class within cpg
+     * @param cpg The CodePropertyGraph containing all existing classes and relations
      */
-    private static void collectAllClassNames(CodePropertyGraph cpg,
-                                             ArrayList<String> allClassNames) {
+    private static List<String> collectAllClassNames(CodePropertyGraph cpg) {
         HashSet<String> uniqueNames = new HashSet<>();
         cpg.getClasses().forEach(cpgClass -> uniqueNames.add(cpgClass.name));
         cpg.getClasses().forEach(cpgClass -> uniqueNames.add(cpgClass.classFullName));
-        allClassNames.addAll(uniqueNames);
+        List<String> allClassNames = new ArrayList<>(uniqueNames);
+        return Collections.unmodifiableList(allClassNames);
     }
 
+
+    /**
+     * Collect all the methods that exist within cpg.
+     *
+     * @param cpg The CodePropertyGraph containing all existing classes and relations
+     */
+    private static List<CPGClass.Method> collectAllMethods(CodePropertyGraph cpg) {
+        List<CPGClass.Method> allMethods = new ArrayList<>();
+        cpg.getClasses().forEach(cpgClass -> allMethods.addAll(cpgClass.getMethods()));
+        return Collections.unmodifiableList(allMethods);
+    }
+
+    /**
+     * Collect all the method names that exist within the cpg into one single list.
+     *
+     * @param cpg The CodePropertyGraph containing all existing classes and relations
+     * @return
+     */
+    private static List<String> collectAllMethodNames(CodePropertyGraph cpg) {
+        List<String> allMethodNames = new ArrayList<>();
+        cpg.getClasses().forEach(cpgClass -> cpgClass.getMethods().forEach(method -> allMethodNames.add(method.name)));
+        return Collections.unmodifiableList(allMethodNames);
+    }
+
+    /**
+     * Collect all the attribute calls of each method into one single list.
+     *
+     * @param allMethods All the methods within cpg
+     * @return
+     */
+    private static List<CPGClass.Attribute> collectAllAttributeCalls(List<CPGClass.Method> allMethods) {
+        List<CPGClass.Attribute> allAttributeCalls = new ArrayList<>();
+        allMethods.forEach(method -> allAttributeCalls.addAll(method.getAttributeCalls()));
+        return Collections.unmodifiableList(allAttributeCalls);
+    }
+
+    /**
+     * Collect all the method calls of each method into one single list.
+     *
+     * @param allMethods All the methods within cpg
+     * @return
+     */
+    private static List<CPGClass.Method> collectAllMethodCalls(List<CPGClass.Method> allMethods) {
+        List<CPGClass.Method> allMethodCalls = new ArrayList<>();
+        allMethods.forEach(method -> allMethodCalls.addAll(method.getMethodCalls()));
+        return Collections.unmodifiableList(allMethodCalls);
+    }
+
+
+    /**
+     * Collect all the method parameters of each method into one single list.
+     *
+     * @param allMethods All the methods within cpg
+     */
+    private static List<CPGClass.Method.Parameter> collectAllParameters(List<CPGClass.Method> allMethods) {
+        List<CPGClass.Method.Parameter> allParameters = new ArrayList<>();
+        allMethods.forEach(method -> allParameters.addAll((method.parameters)));
+        return Collections.unmodifiableList(allParameters);
+    }
 
 }
