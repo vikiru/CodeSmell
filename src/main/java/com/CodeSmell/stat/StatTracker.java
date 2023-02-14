30ee3a15d9f61e219aca510e5527567a74d43551
vikiru <viskirubakaran@cmail.carleton.ca>
2023-02-14 01:59:49 -0500

    Update StatTracker.java
    - cleanup
    - switch void to return
    - switch to unmodifiable collections, prefer interface over concrete implementation

--- src/main/java/com/CodeSmell/stat/StatTracker.java
+++ src/main/java/com/CodeSmell/stat/StatTracker.java
@@ -4,9 +4,7 @@ import com.CodeSmell.model.ClassRelation;
 import com.CodeSmell.parser.CPGClass;
 import com.CodeSmell.parser.CodePropertyGraph;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.TreeMap;
+import java.util.*;
 import java.util.stream.Collectors;
 
 /**
@@ -19,97 +17,98 @@ public final class StatTracker {
     /**
      * Group all {@link CPGClass} by their classType.
      */
-    public final TreeMap<String, ArrayList<CPGClass>> distinctClassTypes = new TreeMap<>();
+    public final Map<String, List<CPGClass>> distinctClassTypes;
 
     /**
      * Group all the relations present within cpg by their {@link com.CodeSmell.model.ClassRelation.RelationshipType}
      */
-    public final HashMap<ClassRelation.RelationshipType, ArrayList<CodePropertyGraph.Relation>> distinctRelations = new HashMap<>();
+    public final Map<ClassRelation.RelationshipType, List<CodePropertyGraph.Relation>> distinctRelations;
 
     /**
      * Group all the {@link CPGClass} with a {@link ClassStat} containing statistics about that class and its
      * attributes and methods via {@link AttributeStat} and {@link MethodStat} respectively
      */
-    public final HashMap<CPGClass, ClassStat> classStats = new HashMap<>();
+    public final Map<CPGClass, ClassStat> classStats;
 
-    public final HashMap<String, Integer> packageUse = new HashMap<>();
-    public final ArrayList<CPGClass.Method> longParameterMethod = new ArrayList<>();
-    public final ArrayList<CPGClass.Method> longMethods = new ArrayList<>();
+    public final Map<String, Integer> packageUse;
+    public final List<CPGClass.Method> longParameterMethod;
+    public final List<CPGClass.Method> longMethods;
 
     public StatTracker(CodePropertyGraph cpg) {
         helper = new Helper(cpg);
-        createStatObjects(cpg, helper, classStats);
-        determineDistinctClassTypes(cpg, distinctClassTypes);
-        determineDistinctRelations(cpg, distinctRelations);
-        determinePackageUsage(classStats, packageUse);
-        findLongParameterMethods(helper, longParameterMethod, 4);
-        findLongMethods(helper, longMethods, 30);
+        this.distinctClassTypes = determineDistinctClassTypes(cpg);
+        this.distinctRelations = determineDistinctRelations(cpg);
+        this.classStats = createStatObjects(cpg, helper);
+        this.packageUse = determinePackageUsage(classStats);
+        this.longParameterMethod = findLongParameterMethods(helper, 4);
+        this.longMethods = findLongMethods(helper, 30);
     }
 
     /**
      * Iterate through the cpg to create stat objects corresponding to elements within the graph.
      *
-     * @param cpg        The CodePropertyGraph containing all existing classes and relations
-     * @param helper     The helper consisting of useful collections of elements within cpg
-     * @param classStats A map containing the statistics of each class within cpg
+     * @param cpg    The CodePropertyGraph containing all existing classes and relations
+     * @param helper The helper consisting of useful collections of elements within cpg
+     * @return
      */
-    private static void createStatObjects(CodePropertyGraph cpg, Helper helper, HashMap<CPGClass, ClassStat> classStats) {
+    private static Map<CPGClass, ClassStat> createStatObjects(CodePropertyGraph cpg, Helper helper) {
+        Map<CPGClass, ClassStat> classStats = new HashMap<>();
         for (CPGClass cpgClass : cpg.getClasses()) {
             classStats.put(cpgClass, new ClassStat(cpgClass, cpg, helper));
         }
+        return Collections.unmodifiableMap(classStats);
     }
 
     /**
      * Iterates through the cpg and groups all relations by RelationshipType.
      *
-     * @param cpg               The CodePropertyGraph containing all existing classes and relations
-     * @param distinctRelations The map containing all the relations grouped by their type
+     * @param cpg The CodePropertyGraph containing all existing classes and relations
+     * @return
      */
-    private static void determineDistinctRelations(CodePropertyGraph cpg,
-                                                   HashMap<ClassRelation.RelationshipType,
-                                                           ArrayList<CodePropertyGraph.Relation>> distinctRelations) {
+    private static Map<ClassRelation.RelationshipType, List<CodePropertyGraph.Relation>> determineDistinctRelations(CodePropertyGraph cpg) {
+        Map<ClassRelation.RelationshipType, List<CodePropertyGraph.Relation>> distinctRelations = new HashMap<>();
         for (ClassRelation.RelationshipType relationshipType : ClassRelation.RelationshipType.values()) {
             var allMatchingRelations = cpg.getRelations().stream().
                     filter(relation -> relation.type.equals(relationshipType)).collect(Collectors.toList());
-            distinctRelations.put(relationshipType, (ArrayList<CodePropertyGraph.Relation>) allMatchingRelations);
+            distinctRelations.put(relationshipType, allMatchingRelations);
         }
+        return Collections.unmodifiableMap(distinctRelations);
     }
 
     /**
      * Iterates through the cpg to determine all the distinct class types that are present such as
      * abstract class, class, enum, interface.
      *
-     * @param cpg                The CodePropertyGraph containing all existing classes and relations
-     * @param distinctClassTypes The map containing all the classes grouped by their type
+     * @param cpg The CodePropertyGraph containing all existing classes and relations
+     * @return
      */
-    private static void determineDistinctClassTypes(CodePropertyGraph cpg,
-                                                    TreeMap<String, ArrayList<CPGClass>> distinctClassTypes) {
+    private static Map<String, List<CPGClass>> determineDistinctClassTypes(CodePropertyGraph cpg) {
+        Map<String, List<CPGClass>> distinctClassTypes = new TreeMap<>();
         for (CPGClass cpgClass : cpg.getClasses()) {
             String classType = cpgClass.classType;
             distinctClassTypes.putIfAbsent(classType, new ArrayList<>());
             distinctClassTypes.get(classType).add(cpgClass);
         }
+        return Collections.unmodifiableMap(distinctClassTypes);
     }
 
-    private static void determinePackageUsage(HashMap<CPGClass, ClassStat> classStats, HashMap<String, Integer> packageUse) {
-        ArrayList<ClassStat> classStatVals = new ArrayList<>(classStats.values());
+    private static Map<String, Integer> determinePackageUsage(Map<CPGClass, ClassStat> classStats) {
+        Map<String, Integer> packageUse = new HashMap<>();
+        List<ClassStat> classStatVals = new ArrayList<>(classStats.values());
         classStatVals.forEach(classStat ->
                 packageUse.put(classStat.cpgClass.packageName,
                         packageUse.getOrDefault((classStat.cpgClass.packageName), 0) + classStat.classUsage));
+        return Collections.unmodifiableMap(packageUse);
     }
 
-    private static void findLongParameterMethods(Helper helper, ArrayList<CPGClass.Method> longParameterMethod, int limit) {
-        var longParamResult = helper.allMethods.stream().
-                filter(method -> method.parameters.size() >= limit).distinct().
-                collect(Collectors.toList());
-        longParameterMethod.addAll(longParamResult);
+    private static List<CPGClass.Method> findLongParameterMethods(Helper helper, int limit) {
+        return helper.allMethods.stream().
+                filter(method -> method.parameters.size() >= limit).distinct().collect(Collectors.toUnmodifiableList());
     }
 
-    private static void findLongMethods(Helper helper, ArrayList<CPGClass.Method> longMethods, int limit) {
-        var longMethodsResult = helper.allMethods.stream().
-                filter(method -> method.totalMethodLength >= limit).distinct().
-                collect(Collectors.toList());
-        longMethods.addAll(longMethodsResult);
+    private static List<CPGClass.Method> findLongMethods(Helper helper, int limit) {
+        return helper.allMethods.stream().
+                filter(method -> method.totalMethodLength >= limit).distinct().collect(Collectors.toUnmodifiableList());
     }
 
 }
