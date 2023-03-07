cd80cc69fb18bf957a056ec8a3155f62d41ac055
Martin <martin.rivard@teksavvy.com>
2023-03-07 13:02:23 -0500

    Trim out unnecessary lines from ShotgunSurgery.

--- src/main/java/com/CodeSmell/smell/ShotgunSurgery.java
+++ src/main/java/com/CodeSmell/smell/ShotgunSurgery.java
@@ -25,24 +25,16 @@ public class ShotgunSurgery extends Smell {
     private void detectAll(){
         ArrayList<CPGClass> classes = cpg.getClasses();
 
-        Map<CPGClass.Method, ArrayList<CPGClass.Method>> callMethods = new HashMap<CPGClass.Method, ArrayList<CPGClass.Method>>();
-        Map<CPGClass.Method, ArrayList<CPGClass>> callClasses = new HashMap<CPGClass.Method, ArrayList<CPGClass>>();
-
-        //Map<CPGClass.Method, CPGClass> methodClasses = new HashMap<CPGClass.Method, CPGClass>();
-
         //first, build the dictionaries
         for(CPGClass curClass : classes){
             for(CPGClass.Method curMethod : curClass.getMethods()){
 
                 MethodStat mStat = new MethodStat(curMethod, cpg, new Helper(cpg));
-
-                int methodCallsSize = 0;
                 ArrayList<CPGClass> classCalls = new ArrayList<>();
                 ArrayList<CPGClass.Method> methodCalls = new ArrayList<>();
                 //build lists of classes this method calls, and number of methods this calls
-
                 for(Map.Entry<CPGClass, List<CPGClass.Method>> entry : mStat.distinctMethodCalls.entrySet()){
-                    methodCallsSize += entry.getValue().size();
+
                     for(CPGClass.Method m : entry.getValue()){
                         methodCalls.add(m);
                         if(!classCalls.contains(m.getParent())){
@@ -50,13 +42,11 @@ public class ShotgunSurgery extends Smell {
                         }
                     }
                 }
-                //System.out.println("   distinctMethodCalls size: " + methodCallsSize);
-                //System.out.println("   classCalls size: " + classCalls.size());
-                if((methodCallsSize >= CALLING_METHOD_CAP)
+                //check to see if it hits the main thresholds, if so, add a smell
+                if((methodCalls.size() >= CALLING_METHOD_CAP)
                     && (classCalls.size() >= CALLING_CLASS_CAP)){
                     this.addSmell(curMethod, methodCalls, classCalls, curClass);
                 }
-
             }
         }
     }
@@ -66,7 +56,7 @@ public class ShotgunSurgery extends Smell {
      * @param method the Method in question that has the shotgun surgery smell
      */
     private void addSmell(CPGClass.Method method, ArrayList<CPGClass.Method> callMethods, ArrayList<CPGClass> callClasses, CPGClass parentClass){
-        String description = method.getParent().classFullName + "." + method.name + " may have shotgun surgery.\n"
+        String description = method.getParent().classFullName + "." + method.name + " may have shotgun surgery, making it difficult.\n"
                 + method.name + " references " + callClasses.size() + " classes and " + callMethods.size() + " methods.\n"
                 + "It may make use of too many different functions and classes to define its functionality.";
         CodeFragment fragment = new CodeFragment(description, new CPGClass[]{parentClass}, new CPGClass.Method[]{method}, null, null, null, null);
@@ -75,6 +65,6 @@ public class ShotgunSurgery extends Smell {
 
     @Override
     public String description() {
-        return "A feature that is implemented or used in many different locations simultaneously.";
+        return "A feature whose behavior is defined in many different classes and methods, making it difficult to modify.";
     }
 }
